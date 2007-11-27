d1c8764cd0738d411c8c787dcdb81ea922d3bef4
Johann A. Briffa <j.briffa@surrey.ac.uk>
2007-11-27 14:56:35 +0000

    In watermarkcode: * optimized demodulate() by pre-computing loop limits * fixed a bug in loop limits, since drift limits were incorrect

--- Libraries/Libcomm/Source/watermarkcode.cpp
+++ Libraries/Libcomm/Source/watermarkcode.cpp
@@ -199,12 +199,18 @@ template <class real> void watermarkcode<real>::demodulate(const channel& chan, 
          {
          ptable(i,d) = 0;
          // In loop below skip out-of-bounds cases:
-         // 1. received vector size: x2-x1+n >= 0
-         // 2. drift introduced in this section: abs(x2-x1) <= xmax
-         // 3. first bit of received vector must exist: n*i+x1 >= 0
-         // 4. last bit of received vector must exist: n*(i+1)+x2 <= rx.size()
-         for(int x1=max(-xmax,-n*i); x1<=xmax; x1++)
-            for(int x2=max(-xmax,x1-n); x2<=min(min(xmax,x1+xmax),rx.size()-n*(i+1)); x2++)
+         // 1. first bit of received vector must exist: n*i+x1 >= 0
+         // 2. last bit of received vector must exist: n*(i+1)+x2-1 <= rx.size()-1
+         // 3. received vector size: x2-x1+n >= 0
+         // 4. drift introduced in this section: -n <= x2-x1 <= n*I
+         //    [first part corresponds to requirement 3]
+         const int x1min = max(-xmax,-n*i);
+         const int x1max = xmax;
+         for(int x1=x1min; x1<=x1max; x1++)
+            {
+            const int x2min = max(-xmax,x1-n);
+            const int x2max = min(min(xmax,x1+n*I),rx.size()-n*(i+1));
+            for(int x2=x2min; x2<=x2max; x2++)
                {
                // create the considered transmitted sequence
                libbase::vector<sigspace> tx(n);
@@ -228,6 +234,7 @@ template <class real> void watermarkcode<real>::demodulate(const channel& chan, 
                   }
 #endif
                }
+            }
          }
       }
    trace << "DEBUG (watermarkcode::demodulate): ptable done.\n";
--- Libraries/Libcomm/Source/watermarkcode.h
+++ Libraries/Libcomm/Source/watermarkcode.h
@@ -76,13 +76,15 @@
   Version 1.25 (13 Nov 2007)
   * optimization of demodulate()
 
-  Version 1.26 (26 Nov 2007)
+  Version 1.26 (26-27 Nov 2007)
   * fixed a bug in modulation, where the incorrect index was applied to the tx vector
   * added debugging information printing during modulation, when working with small blocks
   * added debugging information printing during demodulation, when working with small blocks
   * optimized demodulate() by removing the copying operation on the received sequence
   * fixed a bug in demodulation, where the received vector being considered was
     incorrectly assumed to consist of one bit rather than 'n'.
+  * optimized demodulate() by pre-computing loop limits
+  * fixed a bug in loop limits, since drift limits were incorrect
 */
 
 namespace libcomm {
