529fd75d81ef0bf37b8384134cf96954f9b81afe
Johann A. Briffa <j.briffa@surrey.ac.uk>
2007-12-12 12:07:45 +0000

    Implemented multiplication

--- Libraries/Libbase/Source/gf.cpp
+++ Libraries/Libbase/Source/gf.cpp
@@ -47,8 +47,39 @@ template <int m, int poly> gf<m,poly>& gf<m,poly>::operator+=(const gf<m,poly>& 
    return *this;
    }
 
+/*!
+   \brief Multiplication
+   \param   x  Field element we want to multiply to this one (ie. multiplicand).
+
+   Multiplication within extensions of a field is the multiplication of the polynomials
+   representing the two values. This can be done by the usual long-multiplication
+   algorithm. Every time the result overflows, we need to subtract the modular polynomial;
+   for extensions of a binary field, this is achieved by an XOR operation.
+
+   [cf. Gladman, "A Specification for Rijndael, the AES Algorithm", 2003, pp.3-4]
+*/
 template <int m, int poly> gf<m,poly>& gf<m,poly>::operator*=(const gf<m,poly>& x)
    {
+   // Copy the multiplier (A) and multiplicand (B)
+   int32u A = value;
+   int32u B = x.value;
+   // Initialize result
+   value = 0;
+   // Loop over all bits in multiplicand
+   for(int i=0; i<m || B==0; i++)
+      {
+      // If the corresponding bit in the multiplicand is set,
+      // add (XOR) the shifted multiplier
+      if(B & 1)
+         value ^= A;
+      // Shift the multiplicand
+      B >>= 1;
+      // Shift the multiplier, subtracting the polynomial on overflow
+      A <<= 1;
+      if(A & (1<<m))
+         A ^= poly;
+      }
+   return *this;
    }
 
 // *** Non-member functions
