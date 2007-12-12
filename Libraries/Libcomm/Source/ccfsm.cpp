c040a6a9f3ddedb32afbe79f49268538914e46c6
Johann A. Briffa <j.briffa@surrey.ac.uk>
2007-12-12 17:04:34 +0000

    Added non-binary fsm core

---
+++ Libraries/Libcomm/Source/ccfsm.cpp
@@ -0,0 +1,165 @@
+#include "ccfsm.h"
+#include <iostream>
+#include <sstream>
+
+namespace libcomm {
+
+using libbase::vector;
+
+// Internal functions
+
+/*! \brief Initialization
+*/
+template <class G> void ccfsm<G>::init(const libbase::matrix<vector<G>>& generator)
+   {
+   // copy automatically what we can
+   gen = generator;
+   k = gen.xsize();
+   n = gen.ysize();
+   // set default value to the rest
+   m = 0;
+   // check that the generator matrix is valid (correct sizes) and create shift registers
+   reg.init(k);
+   nu = 0;
+   for(int i=0; i<k; i++)
+      {
+      // assume with of register of input 'i' from its generator sequence for first output
+      int m = gen(i,0).size() - 1;
+      reg(i).init(m);
+      nu += m;
+      // check that the gen. seq. for all outputs are the same length
+      for(int j=1; j<n; j++)
+         if(gen(i,j).size() != m+1)
+            {
+            std::cerr << "FATAL ERROR (ccfsm): Generator sequence must have constant width for each input.\n";
+            exit(1);
+            }
+      // update memory order
+      if(m > ccfsm::m)
+         ccfsm::m = m;
+      }
+   }
+
+
+// Constructors / Destructors
+
+/*! \brief Principal constructor
+*/
+template <class G> ccfsm<G>::ccfsm(const libbase::matrix<vector<G>>& generator)
+   {
+   init(generator);
+   }
+
+/*! \brief Copy constructor
+*/
+template <class G> ccfsm<G>::ccfsm(const ccfsm& x)
+   {
+   // copy automatically what we can
+   k = x.k;
+   n = x.n;
+   nu = x.nu;
+   m = x.m;
+   gen = x.gen;
+   reg = x.reg;
+   }
+
+
+// FSM state operations (getting and resetting)
+
+/*! \brief Returns the current state
+*/
+template <class G> int ccfsm<G>::state() const
+   {
+   vector<G> newstate;
+   newstate.init(0);
+   for(int i=0; i<k; i++)
+      newstate = reg(i) + newstate;
+   return newstate;
+   }
+
+/*! \brief Reset to a specified state
+*/
+template <class G> void ccfsm<G>::reset(int state)
+   {
+   vector<G> newstate;
+   newstate.resize(nu);
+   newstate = state;
+   for(int i=0; i<k; i++)
+      {
+      int size = reg(i).size();
+      // check for case where no memory is associated with the input bit
+      if(size > 0)
+         {
+         reg(i) = newstate.extract(size-1, 0);
+         newstate >>= size;
+         }
+      }
+   }
+
+
+// FSM operations (advance/output/step)
+
+/*! \brief Feeds the specified input and advances the state
+*/
+template <class G> void ccfsm<G>::advance(int& input)
+   {
+   vector<G> ip = determineinput(input);
+   vector<G> sin = determinefeedin(ip);
+   // Update input
+   input = ip;
+   // Compute next state
+   for(int i=0; i<k; i++)
+      reg(i) = sin[i] >> reg(i);
+   }
+
+/*! \brief Computes the output for the given input and the present state
+*/
+template <class G> int ccfsm<G>::output(const int& input) const
+   {
+   vector<G> ip = determineinput(input);
+   vector<G> sin = determinefeedin(ip);
+   // Compute output
+   vector<G> op(0,0);
+   for(int j=0; j<n; j++)
+      {
+      vector<G> thisop(0,1);
+      for(int i=0; i<k; i++)
+         thisop ^= (sin[i] + reg(i)) * gen(i,j);
+      op = thisop + op;
+      }
+   return op;
+   }
+
+
+// Description & Serialization
+
+/*! \brief Description output - common part only, must be preceded by specific name
+*/
+template <class G> std::string ccfsm<G>::description() const
+   {
+   std::ostringstream sout;
+   sout << "(nu=" << nu << ", rate " << k << "/" << n << ", G=[";
+   for(int i=0; i<k; i++)
+      for(int j=0; j<n; j++)
+         sout << gen(i,j) << (j==n-1 ? (i==k-1 ? "])" : "; ") : ", ");
+   return sout.str();
+   }
+
+/*! \brief Serialization output
+*/
+template <class G> std::ostream& ccfsm<G>::serialize(std::ostream& sout) const
+   {
+   sout << gen;
+   return sout;
+   }
+
+/*! \brief Serialization input
+*/
+template <class G> std::istream& ccfsm<G>::serialize(std::istream& sin)
+   {
+   sin >> gen;
+   init(gen);
+   return sin;
+   }
+
+}; // end namespace
---
+++ Libraries/Libcomm/Source/ccfsm.h
@@ -0,0 +1,86 @@
+#ifndef __ccfsm_h
+#define __ccfsm_h
+
+#include "vcs.h"
+#include "fsm.h"
+#include "matrix.h"
+#include "vector.h"
+
+namespace libcomm {
+
+/*!
+   \brief   Controller-Canonical Finite State Machine.
+   \author  Johann Briffa
+
+   \par Version Control:
+   - $Revision$
+   - $Date$
+   - $Author$
+
+   \version 1.00 (11 Dec 2007)
+   - Initial version; implements common elements of a controller-canonical fsm,
+     where each coefficient is an element in a finite field.
+   - The finite field is specified as a template parameter.
+*/
+
+template <class G> class ccfsm : public fsm {
+protected:
+   /*! \name Object representation */
+   int k;   //!< Number of inputs (symbols per time-step)
+   int n;   //!< Number of outputs (symbols per time-step)
+   int nu;  //!< Total number of memory elements (constraint length)
+   int m;   //!< Memory order (longest input register)
+   libbase::vector<libbase::vector<G>> reg;  //!< Shift registers (one for each input)
+   libbase::matrix<libbase::vector<G>> gen;  //!< Generator sequence
+   // @}
+private:
+   /*! \name Internal functions */
+   void init(const libbase::matrix<libbase::vector<G>>& generator);
+   // @}
+protected:
+   /*! \name Implementation-dependent functions */
+   virtual libbase::vector<G> determineinput(const int input) const = 0;
+   virtual libbase::vector<G> determinefeedin(const int input) const = 0;
+   // @}
+   /*! \name Constructors / Destructors */
+   //! Default constructor
+   ccfsm() {};
+   // @}
+public:
+   /*! \name Constructors / Destructors */
+   ccfsm(const libbase::matrix<libbase::vector<G>>& generator);
+   ccfsm(const ccfsm& x);
+   ~ccfsm() {};
+   // @}
+   
+   /*! \name FSM state operations (getting and resetting) */
+   int state() const;
+   void reset(int state=0);
+   // @}
+   /*! \name FSM operations (advance/output/step) */
+   void advance(int& input);
+   int output(const int& input) const;
+   // @}
+
+   /*! \name FSM information functions */
+   //! Memory order (length of tail)
+   int mem_order() const { return m; };
+   //! Number of defined states
+   int num_states() const { return 1<<nu; };
+   //! Number of valid inputs
+   int num_inputs() const { return 1<<k; };
+   //! Number of valid outputs
+   int num_outputs() const { return 1<<n; };
+   // @}
+
+   /*! \name Description & Serialization */
+   std::string description() const;
+   std::ostream& serialize(std::ostream& sout) const;
+   std::istream& serialize(std::istream& sin);
+   // @}
+};
+
+}; // end namespace
+
+#endif
+
