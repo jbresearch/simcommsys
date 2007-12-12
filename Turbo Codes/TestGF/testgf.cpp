408664f4b6d2acf19cddebaeef80efcfbc85a91c
Johann A. Briffa <j.briffa@surrey.ac.uk>
2007-12-12 12:28:16 +0000

    Added test program for GF

---
+++ Turbo Codes/TestGF/testgf.cpp
@@ -0,0 +1,19 @@
+/*!
+   \file
+
+   \par Version Control:
+   - $Revision: 158 $
+   - $Date: 2007-12-12 12:09:52 +0000 (Wed, 12 Dec 2007) $
+   - $Author: jabriffa $
+*/
+
+#include "gf.h"
+#include <iostream>
+
+int main(int argc, char *argv[])
+   {
+   using std::cout;
+   using std::cerr;
+   
+   return 0;
+   }
