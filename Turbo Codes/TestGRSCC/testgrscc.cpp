c7db3ba2bb414e64408bc7f0e8da70da85b7774f
Johann A. Briffa <j.briffa@surrey.ac.uk>
2007-12-14 17:41:30 +0000

    Included test to compare rscc with grscc<GF2>

--- Turbo Codes/TestGRSCC/testgrscc.cpp
+++ Turbo Codes/TestGRSCC/testgrscc.cpp
@@ -7,24 +7,30 @@
    - $Author$
 */
 
+#include "rscc.h"
 #include "grscc.h"
 #include "gf.h"
 #include <iostream>
 
-int main(int argc, char *argv[])
+using std::cout;
+using std::cerr;
+
+using libbase::vector;
+using libbase::matrix;
+
+using libbase::gf;
+using libcomm::grscc;
+using libbase::bitfield;
+using libcomm::rscc;
+
+// Define types for binary and for GF(2^4): m(x) = 1 { 0011 }
+typedef gf<1,0x3>  GF2;
+typedef gf<4,0x13> GF16;
+
+matrix<vector<GF16>> GetGeneratorGF16()
    {
-   using std::cout;
-   using std::cerr;
-
-   using libbase::vector;
-   using libbase::matrix;
-
-   // Define type for RSC code in GF(2^4): m(x) = 1 { 0011 }
-   typedef libbase::gf<4,0x13> G;
-   typedef libcomm::grscc<G> RSC;
-
    // Create generator matrix for a R=1/3 code
-   matrix< vector<G> > gen(1,3);
+   matrix< vector<GF16> > gen(1,3);
    // 1 + D + a^4 D^2
    gen(0,0).init(3);
    gen(0,0)(2) = "0001";
@@ -40,27 +46,75 @@ int main(int argc, char *argv[])
    gen(0,2)(2) = "0001";
    gen(0,2)(1) = "0100";
    gen(0,2)(0) = "1010";
-   // Display generator matrix
-   cout << "Generator matrix:\n";
-   cout << gen;
+   return gen;
+   }
 
-   // Create RSC code from this generator matrix
-   RSC cc(gen);
+matrix<vector<GF2>> GetGeneratorGF2()
+   {
+   // Create generator matrix for a R=1/2 code
+   matrix< vector<GF2> > gen(1,2);
+   // 1 + D + D^2
+   gen(0,0).init(3);
+   gen(0,0)(2) = "1";
+   gen(0,0)(1) = "1";
+   gen(0,0)(0) = "1";
+   // 1 + D^2
+   gen(0,1).init(3);
+   gen(0,1)(2) = "1";
+   gen(0,1)(1) = "0";
+   gen(0,1)(0) = "1";
+   return gen;
+   }
+
+matrix<bitfield> GetGeneratorBinary()
+   {
+   // Create generator matrix for a R=1/2 code
+   matrix<bitfield> gen(1,2);
+   // 1 + D + D^2
+   gen(0,0) = "111";
+   // 1 + D^2
+   gen(0,1) = "101";
+   return gen;
+   }
+
+int main(int argc, char *argv[])
+   {
+   // Create RSC code from generator matrix for R=1/3, nu=3, GF(16)
+   grscc<GF16> cc(GetGeneratorGF16());
    // Show code description
    cout << "Code description:\n";
    cout << cc.description() << "\n";
    // Show code serialization
    cout << "Code serialization: [" << &cc << "]\n";
 
-   // Compute and display exponential table using {03} as a multiplier
-   // using the tabular format in Gladman.
-   //for(int x=0; x<16; x++)
-   //   for(int y=0; y<16; y++)
-   //      {
-   //      assert(E == table[(x<<4)+y]);
-   //      cout << std::hex << int(E) << (y==15 ? '\n' : '\t');
-   //      E *= 3;
-   //      }
+   // Compute, display, and compare the state table for an RSC with G = [111,101]
+   // between the classic rscc and the grscc using the degenerate binary field
+   rscc cc_old(GetGeneratorBinary());
+   cout << "Classic Code description:\n";
+   cout << cc_old.description() << "\n";
+   
+   grscc<GF2> cc_new(GetGeneratorGF2());
+   cout << "New Code description:\n";
+   cout << cc_new.description() << "\n";
+
+   cout << "PS\tIn\tNS\tOut\n";
+   for(int q=0; q<cc_old.num_states(); q++)
+      for(int i=0; i<cc_old.num_inputs(); i++)
+         {
+         cout << q << '\t';
+         cout << i << '\t';
+         cc_old.reset(q);
+         const int out = cc_old.step(i);
+         const int ns = cc_old.state();
+         cout << ns << '\t';
+         cout << out << '\n';
+         cc_new.reset(q);
+         assert(q == cc_new.state());
+         const int n_out = cc_new.step(i);
+         const int n_ns = cc_new.state();
+         assert(out == n_out);
+         assert(ns == n_ns);
+         }
 
    return 0;
    }
