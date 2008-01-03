5beca5b0de02f3fdf5f99db19f41743874d6fe53
Johann A. Briffa <j.briffa@surrey.ac.uk>
2008-01-03 15:43:15 +0000

    Added test program for modems

---
+++ Turbo Codes/TestModem/testmodem.cpp
@@ -0,0 +1,25 @@
+/*!
+   \file
+
+   \par Version Control:
+   - $Revision: 280 $
+   - $Date: 2008-01-02 15:10:32 +0000 (Wed, 02 Jan 2008) $
+   - $Author: jabriffa $
+*/
+
+#include "mpsk.h"
+#include "qam.h"
+#include "bitfield.h"
+#include <iostream>
+
+using libbase::bitfield;
+
+using std::cout;
+using std::cerr;
+using std::hex;
+using std::dec;
+
+int main(int argc, char *argv[])
+   {
+   return 0;
+   }
