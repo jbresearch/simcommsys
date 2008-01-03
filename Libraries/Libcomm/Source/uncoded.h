293c2a75f77ca92ddd293d2de0d302c7b94b21da
Johann A. Briffa <j.briffa@surrey.ac.uk>
2008-01-03 10:53:19 +0000

    Cleaned up formatting

--- Libraries/Libcomm/Source/uncoded.h
+++ Libraries/Libcomm/Source/uncoded.h
@@ -21,47 +21,47 @@ namespace libcomm {
    - $Author$
 
    \version 1.01 (5 Mar 1999)
-  renamed the mem_order() function to tail_length(), which is more indicative of the true
-  use of this function (complies with codec 1.01).
+   renamed the mem_order() function to tail_length(), which is more indicative of the true
+   use of this function (complies with codec 1.01).
 
    \version 1.10 (7 Jun 1999)
-  modified the system to comply with codec 1.10.
+   modified the system to comply with codec 1.10.
 
    \version 1.02 (4 Nov 2001)
-  added a function which outputs details on the codec (in accordance with codec 1.20)
+   added a function which outputs details on the codec (in accordance with codec 1.20)
 
    \version 1.03 (23 Feb 2002)
-  added flushes to all end-of-line clog outputs, to clean up text user interface.
+   added flushes to all end-of-line clog outputs, to clean up text user interface.
 
    \version 1.04 (6 Mar 2002)
-  changed vcs version variable from a global to a static class variable.
-  also changed use of iostream from global to std namespace.
+   changed vcs version variable from a global to a static class variable.
+   also changed use of iostream from global to std namespace.
 
    \version 1.10 (13 Mar 2002)
-  updated the system to conform with the completed serialization protocol (in conformance
-  with codec 1.30), by adding the necessary name() function, and also by adding a static
-  serializer member and initialize it with this class's name and the static constructor
-  (adding that too, together with the necessary protected default constructor). Also made
-  the codec object a public base class, rather than a virtual public one, since this
-  was affecting the transfer of virtual functions within the class (causing access
-  violations). Also added a function which deallocates all heap variables.
+   updated the system to conform with the completed serialization protocol (in conformance
+   with codec 1.30), by adding the necessary name() function, and also by adding a static
+   serializer member and initialize it with this class's name and the static constructor
+   (adding that too, together with the necessary protected default constructor). Also made
+   the codec object a public base class, rather than a virtual public one, since this
+   was affecting the transfer of virtual functions within the class (causing access
+   violations). Also added a function which deallocates all heap variables.
 
    \version 1.20 (17 Mar 2002)
-  modified to conform with codec 1.40.
-  Also, removed the clog & cerr information output during initialization.
+   modified to conform with codec 1.40.
+   Also, removed the clog & cerr information output during initialization.
 
    \version 1.21 (18 Mar 2002)
-  modified to conform with codec 1.41.
+   modified to conform with codec 1.41.
 
    \version 1.30 (27 Mar 2002)
-  changed descriptive output function to conform with codec 1.50.
+   changed descriptive output function to conform with codec 1.50.
 
    \version 1.31 (17 Jul 2006)
-  in translate, made an explicit conversion of round's output to int, to conform with the
-  changes in itfunc 1.07.
+   in translate, made an explicit conversion of round's output to int, to conform with the
+   changes in itfunc 1.07.
 
    \version 1.32 (6 Oct 2006)
-  modified for compatibility with VS .NET 2005:
+   modified for compatibility with VS .NET 2005:
    - in modulate, modified use of pow to avoid ambiguity
 
    \version 1.40 (6 Nov 2006)
@@ -75,9 +75,10 @@ namespace libcomm {
 class uncoded : public codec {
    static const libbase::serializer shelper;
    static void* create() { return new uncoded; };
-   fsm             *encoder;
-   int                  tau;            // block length
-   int                  K, N;              // # of inputs and outputs (respectively)
+   fsm   *encoder;
+   int   tau;  //!< block length
+   int   K;    //!< number of input symbols
+   int   N;    //!< number of output symbols
    libbase::vector<int> lut;
    libbase::matrix<double> R;
 protected:
