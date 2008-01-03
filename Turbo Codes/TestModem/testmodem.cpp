e60abbefc3704a0527405a48d1d1ccd07d322172
Johann A. Briffa <j.briffa@surrey.ac.uk>
2008-01-03 16:05:49 +0000

    Implemented test for QAM

--- Turbo Codes/TestModem/testmodem.cpp
+++ Turbo Codes/TestModem/testmodem.cpp
@@ -30,6 +30,18 @@ void TestMPSK(int m)
    assert(m == 1<<bits);
    mpsk modem(m);
    cout << '\n' << modem.description() << '\n';
+   cout << "Average Energy/symbol: " << modem.energy() << '\n';
+   for(int i=0; i<m; i++)
+      cout << bitfield(gray(i),bits) << '\t' << modem.modulate(gray(i)) << '\n';
+   }
+
+void TestQAM(int m)
+   {
+   const int bits = int(log2(m));
+   assert(m == 1<<bits);
+   qam modem(m);
+   cout << '\n' << modem.description() << '\n';
+   cout << "Average Energy/symbol: " << modem.energy() << '\n';
    for(int i=0; i<m; i++)
       cout << bitfield(gray(i),bits) << '\t' << modem.modulate(gray(i)) << '\n';
    }
@@ -39,5 +51,7 @@ int main(int argc, char *argv[])
    TestMPSK(2);
    TestMPSK(4);
    TestMPSK(8);
+   TestQAM(4);
+   TestQAM(16);
    return 0;
    }
