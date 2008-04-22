2ef74c633af9d98d5f99d75247bd441772c7b493
Johann A. Briffa <j.briffa@surrey.ac.uk>
2008-04-22 13:44:25 +0000

    Implemented finite-symbol class

--- Libraries/Libcomm/Source/symbol.cpp
+++ Libraries/Libcomm/Source/symbol.cpp
@@ -11,4 +11,16 @@
 
 namespace libcomm {
 
+// Internal functions
+
+/*!
+   \brief Initialization
+   \param   value Integer representation of element
+*/
+template <int q> void finite_symbol<q>::init(int value)
+   {
+   assert(value >=0 && value < (1<<q));
+   finite_symbol::value = value;
+   }
+
 }; // end namespace
--- Libraries/Libcomm/Source/symbol.h
+++ Libraries/Libcomm/Source/symbol.h
@@ -6,7 +6,7 @@
 namespace libcomm {
 
 /*!
-   \brief   Modulation symbol.
+   \brief   Finite symbol base class.
    \author  Johann Briffa
 
    \par Version Control:
@@ -14,17 +14,71 @@ namespace libcomm {
    - $Date$
    - $Author$
 
-   \version 1.00 (22 Jan 2008)
+   \version 1.00 (22 Apr 2008)
    - Initial version.
-   - Created to abstract the concept of a modulation symbol from its signal-space
-     representation.
+   - Created to abstract the concept of a symbol from a finite alphabet.
+   - This is an abstract class which defines the interface to such an object.
 */
 
 class symbol {
 public:
    /*! \name Constructors / Destructors */
-   symbol() {};
-   virtual ~symbol() {};
+   virtual ~symbol() = 0;
+   // @}
+
+   /*! \name Type conversion */
+   virtual operator int() const = 0;
+   virtual symbol& operator=(const int value) = 0;
+   // @}
+
+   /*! \name Class parameters */
+   //! Number of elements in the finite alphabet
+   virtual int elements() const = 0;
+   // @}
+};
+
+
+/*!
+   \brief   Finite q-ary symbol.
+   \author  Johann Briffa
+
+   \par Version Control:
+   - $Revision$
+   - $Date$
+   - $Author$
+
+   \version 1.00 (22 Apr 2008)
+   - Initial version.
+   - Uses an integer to represent symbol value; value is initialized to zero
+     on creation.
+*/
+
+template <int q> class finite_symbol {
+private:
+   /*! \name Object representation */
+   //! Representation of this element by its polynomial coefficients
+   int value;
+   // @}
+
+   /*! \name Internal functions */
+   void init(int value);
+   // @}
+
+public:
+   /*! \name Constructors / Destructors */
+   //! Principal constructor
+   finite_symbol(int value=0) { init(value); };
+   ~finite_symbol() {};
+   // @}
+
+   /*! \name Type conversion */
+   operator int() const { return value; };
+   symbol& operator=(const int value) { init(value); return *this; };
+   // @}
+
+   /*! \name Class parameters */
+   //! Number of elements in the finite alphabet
+   int elements() const { return q; };
    // @}
 };
 
