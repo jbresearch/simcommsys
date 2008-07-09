dcee36da1997e66bcb18ec04c9381006fb90ba07
Johann A. Briffa <j.briffa@surrey.ac.uk>
2008-07-09 09:51:20 +0000

    Updated documentation

--- Libraries/Libcomm/Source/codec.h
+++ Libraries/Libcomm/Source/codec.h
@@ -19,79 +19,9 @@ namespace libcomm {
    - $Date$
    - $Author$
 
-   \version 1.01 (5 Mar 1999)
-  renamed the mem_order() function to tail_length(), which is more indicative of the true
-  use of this function.
-
-   \version 1.10 (7 Jun 1999)
-  modulated sequence changed from matrix to vector, in order to simplify the implementation
-  of puncturing. Now, num_symbols returns the length of the signal space vector.
-
-   \version 1.11 (26 Oct 2001)
-  added a virtual destroy function (see interleaver.h)
-
-   \version 1.20 (4 Nov 2001)
-  added a virtual function which outputs details on the codec (this was only done before
-  in the construction mechanism). Added a stream << operator too.
-
-   \version 1.21 (6 Mar 2002)
-  changed vcs version variable from a global to a static class variable.
-
-   \version 1.30 (13 Mar 2002)
-  added serialization facility. Created serialize and stream << and >> functions to
-  conform with the new serializer protocol, as defined in serializer 1.10. The stream
-  << output function first writes the name of the derived class, then calls its
-  serialize() to output the data. The name is obtained from the virtual name() function.
-  The stream >> input function first gets the name from the stream, then (via
-  serialize::call) creates a new object of the appropriate type and calls its serialize()
-  function to get the relevant data. Also added cloning function.
-
-   \version 1.40 (17 Mar 2002)
-  added information function which returns the number of values each output symbol can
-  take. Also removed the modulate/transmit/demodulate functions (which are now in the
-  modulator and channel modules) and also num_symbols (since this is no longer necessary).
-  Puncturing is now to be performed as a separate step, to avoid the overhead when there
-  is no puncturing. Also, the num_iter function has no default value now (to make sure
-  it's not overlooked). Finally, created a new function 'translate' which computes the
-  necessary prabability tables for the particular codec from the probabilities of each
-  modulation symbol as received from the channel. This function should be called before
-  the first decode iteration for each block. Also, the input vector to encode has been
-  made const.
-
-   \version 1.41 (18 Mar 2002)
-  added three informative functions, giving the number of input bits & output bits per
-  frame, and the data rate for the code. These can be computed from the other information
-  functions for all codes, so they have been implemented as part of the base class. Note
-  that the number of bits need not be an integer (if the codes are not binary).
-  Also made all information functions const. Finally, changed the input vector for
-  encode() back to non-const (tail entries need to be filled in by the encoder with the
-  actual values used).
-
-   \version 1.50 (27 Mar 2002)
-  removed the descriptive output() and related stream << output functions, and replaced
-  them by a function description() which returns a string. This provides the same
-  functionality but in a different format, so that now the only stream << output
-  functions are for serialization. This should make the notation much clearer while
-  also simplifying description display in objects other than streams.
-
-   \version 1.60 (30 Oct 2006)
-   - defined class and associated data within "libcomm" namespace.
-   - removed use of "using namespace std", replacing by tighter "using" statements as needed.
-
-   \version 1.61 (17 Apr 2008)
-   - removed friend status of stream output operators
-
-   \version 1.62 (24 Apr 2008)
-   - replaced serialization support with macros
-
-   \version 1.63 (25 Apr 2008)
-   - added information function to give the required transmission symbol
-     alphabet size (required by mapper); defaults to output alphabet size.
-
-   \todo
-   Change class interface to better model the actual representation of input and
-   output sequences of the codec and to better separate this class from the
-   modulation class.
+   \todo Change class interface to better model the actual representation of
+         input and output sequences of the codec and to better separate this
+         class from the modulation class.
 */
 
 class codec {
@@ -112,12 +42,21 @@ public:
       \note If the input or output symbols at every timestep represent the
             aggregation of a set of symbols, the combination/division has to
             be done externally.
+
+      \note Source is non-const; this is necessary to allow the codec to set
+            values actually used in tail.
    */
    virtual void encode(libbase::vector<int>& source, libbase::vector<int>& encoded) = 0;
    /*!
       \brief Receiver translation process
       \param[in] ptable Matrix representing the likelihoods of each possible
                         modulation symbol at every (modulation) timestep
+
+      This function computes the necessary prabability tables for the codec
+      from the probabilities of each modulation symbol as received from the
+      channel. This function should be called before the first decode iteration
+      for each block.
+
       \note The number of possible modulation symbols does not necessarily
             correspond to the number of encoder output symbols, and therefore
             the number of modulation timesteps may be different from tau.
@@ -141,7 +80,7 @@ public:
    virtual int num_inputs() const = 0;
    //! Number of valid output combinations
    virtual int num_outputs() const = 0;
-   //! Output symbol alphabet size
+   //! Output symbol alphabet size (required by mapper)
    virtual int output_alphabet() const { return num_outputs(); };
    //! Length of tail in timesteps
    virtual int tail_length() const = 0;
@@ -150,9 +89,9 @@ public:
    // @}
 
    /*! \name Codec information functions - derived */
-   //! Length of information sequence in bits
+   //! Equivalent length of information sequence in bits
    double input_bits() const { return log2(num_inputs())*(block_size() - tail_length()); };
-   //! Output block length in bits
+   //! Equivalent length of output sequence in bits
    double output_bits() const { return log2(num_outputs())*block_size(); };
    //! Overall code rate
    double rate() const { return input_bits()/output_bits(); };
--- Libraries/Libcomm/Source/modulator.h
+++ Libraries/Libcomm/Source/modulator.h
@@ -21,11 +21,7 @@ namespace libcomm {
    - $Date$
    - $Author$
 
-   \version 1.00 (24 Jan 2008)
-   - Contains common interface for modulator classes.
-
-   \version 1.01 (22 Feb 2008)
-   - Added seed function.
+   Class defines common interface for modulator classes.
 */
 
 template <class S>
@@ -61,8 +57,11 @@ public:
       \param[in]  encoded  Sequence of values to be modulated
       \param[out] tx       Sequence of symbols corresponding to the given input
 
-      \todo Remove parameter N, replacing 'int' type for encoded vector with something
-            that also encodes the number of symbols in the alphabet
+      \todo Remove parameter N, replacing 'int' type for encoded vector with
+            something that also encodes the number of symbols in the alphabet.
+
+      \note This function is non-const, to support time-variant modulation
+            schemes such as DM inner codes.
    */
    virtual void modulate(const int N, const libbase::vector<int>& encoded, libbase::vector<S>& tx) = 0;
    /*!
@@ -73,6 +72,9 @@ public:
       
       \note \c ptable(i,d) \c is the a posteriori probability of having transmitted 
             symbol 'd' at time 'i'
+
+      \note This function is non-const, to support time-variant modulation
+            schemes such as DM inner codes.
    */
    virtual void demodulate(const channel<S>& chan, const libbase::vector<S>& rx, libbase::matrix<double>& ptable) = 0;
    // @}
@@ -148,89 +150,10 @@ public:
    - $Date$
    - $Author$
 
-   \version 1.01 (26 Oct 2001)
-   added a virtual destroy function (see interleaver.h)
+   Specific implementation of q-ary channel modulation.
 
-   \version 1.02 (6 Mar 2002)
-   changed vcs version variable from a global to a static class variable.
-   also changed use of iostream from global to std namespace.
-
-   \version 1.10 (13 Mar 2002)
-   added a virtual function which outputs details on the modulator, together with a stream
-   << operator too. Also added serialization facility. Created serialize and stream << and
-   >> functions to conform with the new serializer protocol, as defined in serializer 1.10.
-   The stream << output function first writes the name of the derived class, then calls its
-   serialize() to output the data. The name is obtained from the virtual name() function.
-   The stream >> input function first gets the name from the stream, then (via
-   serialize::call) creates a new object of the appropriate type and calls its serialize()
-   function to get the relevant data. Also added cloning function.
-
-   \version 1.20 (17 Mar 2002)
-   added functions to modulate a vector of encoded symbols and demodulate a vector of
-   sigspace symbols (given a channel model), and new names for the functions that modulate
-   or demodulate single elements (the operator[] functions are now just alternative
-   notations). Also modified the information-returning functions to return non-const,
-   since this won't make any difference.
-
-   \version 1.30 (27 Mar 2002)
-   removed the descriptive output() and related stream << output functions, and replaced
-   them by a function description() which returns a string. This provides the same
-   functionality but in a different format, so that now the only stream << output
-   functions are for serialization. This should make the notation much clearer while
-   also simplifying description display in objects other than streams.
-
-   \version 1.31 (27 Mar 2002)
-   changed the way we store the array of modulation symbols from a heap-allocated
-   array to a vector; this significantly simplifies the memory management. Also,
-   removed the member variable M, since the number of symbols is easily obtained from
-   the vector. Also changed modulate() and operator[] to return sigspace object directly
-   not by reference (should have been this way before since the functions are const).
-
-   \version 1.32 (17 Jul 2006)
-   in modulate, made an explicit conversion of round's output to int, to conform with the
-   changes in itfunc 1.07.
-
-   \version 1.33 (6 Oct 2006)
-   modified for compatibility with VS .NET 2005:
-   - in energy_function, modified use of pow to avoid ambiguity
-
-   \version 1.40 (30 Oct 2006)
-   - defined class and associated data within "libcomm" namespace.
-   - removed use of "using namespace std", replacing by tighter "using" statements as needed.
-
-   \version 1.41 (17 Oct 2007)
-   changed class to conform with channel 1.52.
-
-   \version 1.50 (25 Oct 2007)
-   - extracted functionality of static LUT modulators by creating a new class lut_modulator
-   - this class now better reflects its role as a generic base class for the modem layer
-   - removed 'const' restriction on modulate and demodulate vector functions, to cater for watermark codes
-     (where the random generator is updated during these functions); this also introduces support for other
-     time-variant modulation schemes.
-
-   \version 1.51 (30 Nov 2007)
-   - added method to get modulation rate.
-
-   \version 1.52 (22 Jan 2008)
-   - Removed 'friend' declaration of stream operators.
-
-   \version 1.53 (24 Jan 2008)
-   - Changed reference from channel to channel<sigspace>
-
-   \version 2.00 (28 Jan 2008)
-   - Abstracted modulator class by templating, with the channel-symbol type as
-     template parameter; this is meant to allow the use of channels that do not
-     use signal-space transmission.
-   - Signal-space specific functions are moved to a class specialization.
-   - Common modulator interface moved to basic_modulator template.
-   - This class cannot be instantiated as it is still abstract.
-
-   \version 2.01 (12-13 Feb 2008)
-   - Implemented as templated GF(q) modulator - template argument class must
-     provide a method elements() that returns the field size.
-   - Renamed template argument to G
-   - Fixed error in modulate function (symbol splitting was incorrect)
-   - Changed modulate and demodulate to be more similar to those in lut_modulator
+   \note Template argument class must provide a method elements() that returns
+         the field size.
 
    \todo Merge modulate and demodulate between this function and lut_modulator
 */
@@ -265,9 +188,7 @@ public:
    - $Date$
    - $Author$
 
-   \version 1.00 (24 Jan 2008)
-   - Elements specific to the binary channel moved to this implementation
-     derived from the abstract class.
+   Specific implementation of binary channel modulation.
 */
 
 template <>
