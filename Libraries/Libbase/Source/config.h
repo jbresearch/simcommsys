986623252cb6a970ec66967bfb30410248458979
Johann A. Briffa <j.briffa@surrey.ac.uk>
2009-01-21 15:38:27 +0000

    Fixed back for .NET 2005

--- Libraries/Libbase/Source/config.h
+++ Libraries/Libbase/Source/config.h
@@ -89,10 +89,9 @@ inline double sign(double x) {return (x > 0) ? +1 : ((x < 0) ? -1 : 0); }
 
 inline double sqrt(int x) { return sqrt(double(x)); };
 inline double log(int x) { return log(double(x)); };
-#ifndef WIN32
-//inline double pow(double x, int y) { return pow(x,double(y)); };
+#ifdef WIN32
+inline double pow(int x, int y) { return pow(double(x),y); };
 #endif //ifdef WIN32
-//inline double pow(int x, int y) { return pow(double(x),y); };
 
 // Define a function that returns the square of the input
 
