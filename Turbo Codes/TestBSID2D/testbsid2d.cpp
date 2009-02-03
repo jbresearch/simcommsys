e4e5a886fbf47420dedc8770200d628199cd0883
Johann A. Briffa <j.briffa@surrey.ac.uk>
2009-02-03 18:04:51 +0000

    Changed default error probability Added different intial-matrix types

--- Turbo Codes/TestBSID2D/testbsid2d.cpp
+++ Turbo Codes/TestBSID2D/testbsid2d.cpp
@@ -20,14 +20,25 @@ using libbase::matrix;
 using libbase::randgen;
 using libcomm::bsid2d;
 
-void visualtest(int seed=0)
+void visualtest(int seed, int type, double p)
    {
    // define an alternating input sequence
    const int M=5, N=5;
    matrix<bool> tx(M,N);
-   for(int i=0; i<M; i++)
-      for(int j=0; j<N; j++)
-         tx(i,j) = (i&1) ^ (j&1);
+   switch(type)
+      {
+      case 0:
+      case 1:
+         tx = type;
+         break;
+      case 2:
+         for(int i=0; i<M; i++)
+            for(int j=0; j<N; j++)
+               tx(i,j) = (i&1) ^ (j&1);
+         break;
+      default:
+         assertalways("Invalid type");
+      }
    cout << "Tx: " << tx << "\n";
    // pass that through the channel
    matrix<bool> rx1, rx2;
@@ -37,13 +48,13 @@ void visualtest(int seed=0)
    // channel1 is a substitution-only channel
    bsid2d channel1(true,false,false);
    channel1.seedfrom(prng);
-   channel1.set_parameter(0.3);
+   channel1.set_parameter(p);
    channel1.transmit(tx, rx1);
    cout << "Rx1: " << rx1 << "\n";
    // channel1 is an insdel-only channel
    bsid2d channel2(false,true,true);
    channel2.seedfrom(prng);
-   channel2.set_parameter(0.3);
+   channel2.set_parameter(p);
    channel2.transmit(tx, rx2);
    cout << "Rx2: " << rx2 << "\n";
    }
@@ -53,9 +64,12 @@ int main(int argc, char *argv[])
    int seed=0;
    if(argc > 1)
       seed = atoi(argv[1]);
-      
+
    // create a test sequence and test 2D BSID transmission
-   visualtest(seed);
+   const double p = 0.1;
+   visualtest(seed, 0, p); // all-zero sequence
+   visualtest(seed, 1, p); // all-one sequenceee
+   visualtest(seed, 2, p); // random sequence
 
    return 0;
    }
