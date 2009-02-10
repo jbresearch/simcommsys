cc3d8beca391ce08b8c02138b7b0459749cd0055
Johann A. Briffa <j.briffa@surrey.ac.uk>
2009-02-10 17:56:14 +0000

    Added skeleton derivation from informed_modulator

--- Libraries/Libcomm/Source/dminner2d.h
+++ Libraries/Libcomm/Source/dminner2d.h
@@ -4,7 +4,9 @@
 #include "config.h"
 
 #include "dminner2.h"
+#include "informed_modulator.h"
 #include "fba2.h"
+#include "matrix.h"
 
 namespace libcomm {
 
@@ -22,8 +24,35 @@ namespace libcomm {
 */
 
 template <class real, bool normalize>
-class dminner2d {
+class dminner2d :
+   public informed_modulator<bool,libbase::matrix> {
+   //private fba2<real,bool,normalize> {
 public:
+   /*! \name Type definitions */
+   typedef libbase::vector<double>     array1d_t;
+   // @}
+private:
+   // Implementations of channel-specific metrics for fba
+   //real R(const int i, const array1b_t& r);
+   // Atomic modem operations (private as these should never be used)
+   const bool modulate(const int index) const
+      { assert("Function should not be used."); return false; };
+   const int demodulate(const bool& signal) const
+      { assert("Function should not be used."); return 0; };
+   const int demodulate(const bool& signal, const libbase::vector<double>& app) const
+      { assert("Function should not be used."); return 0; };
+protected:
+   // Interface with derived classes
+   //void advance() const;
+   void domodulate(const int N, const libbase::matrix<int>& encoded, libbase::matrix<bool>& tx) {};
+   void dodemodulate(const channel<bool>& chan, const libbase::matrix<bool>& rx, libbase::matrix<array1d_t>& ptable) {};
+   void dodemodulate(const channel<bool>& chan, const libbase::matrix<bool>& rx, const libbase::matrix<array1d_t>& app, libbase::matrix<array1d_t>& ptable) {};
+
+public:
+   
+   // Informative functions
+   int num_symbols() const { return 1; };
+
    // Description
    std::string description() const;
 
