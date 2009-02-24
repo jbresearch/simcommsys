34140a3ee81d4e856a042df9be64ef3a022ba3c3
Johann A. Briffa <j.briffa@surrey.ac.uk>
2009-02-24 13:52:25 +0000

    Initial test - loading and description

--- Turbo Codes/TestDMinner2D/testdminner2d.cpp
+++ Turbo Codes/TestDMinner2D/testdminner2d.cpp
@@ -1,13 +1,11 @@
 #include "logrealfast.h"
-#include "dminner.h"
-#include "bsid.h"
+#include "dminner2d.h"
 #include "itfunc.h"
-#include "bitfield.h"
 
 #include <iostream>
 
 /*!
-   \brief   Test program for DM inner code creation
+   \brief   Test program for 2D DM inner code
    \author  Johann Briffa
 
    \section svn Version Control
@@ -22,39 +20,36 @@ int main(int argc, char *argv[])
    using std::cout;
    using std::cerr;
 
-   // create a watermark code to start with
-   int n=5, k=3;
-   libcomm::dminner<libbase::logrealfast,false> mdm(n,k);
-   cout << mdm.description() << "\n";
-
-   // get a new watermark from stdin
-   cerr << "Enter watermark code details:\n";
+   // create a code to start with
+   libcomm::dminner2d<libbase::logrealfast,false> mdm;
+   // get code definition from stdin
+   cerr << "Enter code details:\n";
    mdm.serialize(cin);
    cout << mdm.description() << "\n";
 
    // compute distance table
-   n = mdm.get_n();
-   k = mdm.get_k();
-   libbase::matrix<int> c(1<<k,n);
-   c = 0;
-   for(int i=0; i<(1<<k); i++)
-      for(int j=i+1; j<(1<<k); j++)
-         {
-         int t = libbase::weight(mdm.get_lut(i) ^ mdm.get_lut(j));
-         c(i,t-1)++;
-         c(j,t-1)++;
-         }
+//    n = mdm.get_n();
+//    k = mdm.get_k();
+//    libbase::matrix<int> c(1<<k,n);
+//    c = 0;
+//    for(int i=0; i<(1<<k); i++)
+//       for(int j=i+1; j<(1<<k); j++)
+//          {
+//          int t = libbase::weight(mdm.get_lut(i) ^ mdm.get_lut(j));
+//          c(i,t-1)++;
+//          c(j,t-1)++;
+//          }
 
    // display codebook and distance table
-   cout << "d\ts\t";
-   for(int t=1; t<=n; t++)
-      cout << "c_" << t << (t==n ? '\n' : '\t');
-   for(int i=0; i<(1<<k); i++)
-      {
-      cout << i << '\t' << libbase::bitfield(mdm.get_lut(i),n) << '\t';
-      for(int t=1; t<=n; t++)
-         cout << c(i,t-1) << (t==n ? '\n' : '\t');
-      }
+//    cout << "d\ts\t";
+//    for(int t=1; t<=n; t++)
+//       cout << "c_" << t << (t==n ? '\n' : '\t');
+//    for(int i=0; i<(1<<k); i++)
+//       {
+//       cout << i << '\t' << libbase::bitfield(mdm.get_lut(i),n) << '\t';
+//       for(int t=1; t<=n; t++)
+//          cout << c(i,t-1) << (t==n ? '\n' : '\t');
+//       }
 
    return 0;
    }
