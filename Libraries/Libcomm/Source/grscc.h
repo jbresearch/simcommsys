13288366978b8b092f99f50512cfc909cc4f8871
Johann A. Briffa <j.briffa@surrey.ac.uk>
2009-04-20 14:55:43 +0000

    Documentation

--- Libraries/Libcomm/Source/grscc.h
+++ Libraries/Libcomm/Source/grscc.h
@@ -15,21 +15,8 @@ namespace libcomm {
    - $Date$
    - $Author$
 
-   \version 1.00 (13 Dec 2007)
-   - Initial version; implements RSCC where polynomial coefficients are elements
-     of a finite field.
-   - Derived from rscc 1.70
-   - The finite field is specified as a template parameter.
-
-   \version 1.01 (4-6 Jan 2008)
-   - removed serialization functions, which were redundant
-   - removed resetcircular(), which is now implemented in fsm()
-   - implemented getstategen() which returns the state-generator matrix, and
-     getstatevec() which returns the state-vector, both in the format required
-     for computing the circulation state
-   - added circulation state correspondence table, and implemented initcsct()
-     which initializes it
-   - implemented resetcircular() using csct
+   Implements RSCC where polynomial coefficients are elements of a finite
+   field, which is specified as a template parameter.
 */
 
 template <class G>
