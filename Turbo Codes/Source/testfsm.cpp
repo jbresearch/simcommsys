970a56e765f90ce1de0f549f3805ae984df23f5d
Johann A. Briffa <j.briffa@surrey.ac.uk>
2009-04-23 12:40:33 +0000

    Added test program for FSMs

---
+++ Turbo Codes/Source/testfsm.cpp
@@ -0,0 +1,1 @@
+../TestFSM/testfsm.cpp
---
+++ Turbo Codes/TestFSM/testfsm.cpp
@@ -0,0 +1,46 @@
+#include "serializer_libcomm.h"
+#include "fsm.h"
+
+#include <iostream>
+
+/*!
+   \brief   Test program for FSM objects
+   \author  Johann Briffa
+
+   \section svn Version Control
+   - $Revision$
+   - $Date$
+   - $Author$
+
+   Serializes a FSM object from standard input; computes and displays
+   state table.
+*/
+
+int main(int argc, char *argv[])
+   {
+   using std::cin;
+   using std::cout;
+   using std::cerr;
+
+   const libcomm::serializer_libcomm my_serializer_libcomm;
+
+   // get a new fsm from stdin
+   cerr << "Enter FSM details:\n";
+   libcomm::fsm *encoder;
+   cin >> encoder;
+   cout << encoder->description() << "\n";
+
+   // compute and display state table
+   cout << "PS\tIn\tOut\tNS\n";
+   for(int ps=0; ps<encoder->num_states(); ps++)
+      for(int in=0; in<encoder->num_inputs(); in++)
+         {
+         cout << ps << '\t';
+         cout << in << '\t';
+         encoder->reset(ps);
+         cout << encoder->step(in) << '\t';
+         cout << encoder->state() << '\n';
+         }
+
+   return 0;
+   }
