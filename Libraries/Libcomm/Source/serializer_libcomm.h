2112275c21f153aa93015605cd06b6c68efff41b
Johann A. Briffa <j.briffa@surrey.ac.uk>
2009-05-07 10:38:21 +0000

    Updated to pull in only one object type for each compilation unit

--- Libraries/Libcomm/Source/serializer_libcomm.h
+++ Libraries/Libcomm/Source/serializer_libcomm.h
@@ -99,14 +99,13 @@ namespace libcomm {
 
 // Serialization support
 class serializer_libcomm : private
-   qsc< libbase::gf<1,0x3> >, qsc< libbase::gf<2,0x7> >, qsc< libbase::gf<3,0xB> >, qsc< libbase::gf<4,0x13> >,
+   qsc< libbase::gf<1,0x3> >,
    awgn, laplacian, lapgauss, bsid, bsid2d, bsc,
    nrcc, rscc, dvbcrsc,
-   grscc< libbase::gf<1,0x3> >, grscc< libbase::gf<2,0x7> >, grscc< libbase::gf<3,0xB> >, grscc< libbase::gf<4,0x13> >,
-   gnrcc< libbase::gf<1,0x3> >, gnrcc< libbase::gf<2,0x7> >, gnrcc< libbase::gf<3,0xB> >, gnrcc< libbase::gf<4,0x13> >,
-   uncoded<double>, mapcc<libbase::logrealfast>, turbo<libbase::logrealfast,libbase::logrealfast>,
-   onetimepad<double>, padded<double>, berrou<double>, flat<double>, helical<double>, rand_lut<double>, rectangular<double>, shift_lut<double>, uniform_lut<double>, named_lut<double>,
-   onetimepad<libbase::logrealfast>, padded<libbase::logrealfast>, berrou<libbase::logrealfast>, flat<libbase::logrealfast>, helical<libbase::logrealfast>, rand_lut<libbase::logrealfast>, rectangular<libbase::logrealfast>, shift_lut<libbase::logrealfast>, uniform_lut<libbase::logrealfast>, named_lut<libbase::logrealfast>
+   grscc< libbase::gf<1,0x3> >,
+   gnrcc< libbase::gf<1,0x3> >,
+   uncoded<double>, mapcc<double>, turbo<double>,
+   onetimepad<double>, padded<double>, berrou<double>, flat<double>, helical<double>, rand_lut<double>, rectangular<double>, shift_lut<double>, uniform_lut<double>, named_lut<double>
 {
 private:
    typedef libbase::logrealfast  logrealfast;
@@ -122,58 +121,25 @@ private:
    //shift_lut<double>	_shift_lut_double;
    //uniform_lut<double>	_uniform_lut_double;
    //named_lut<double>	_named_lut_double;
-   //onetimepad<logrealfast>	_onetimepad_double;
-   //padded<logrealfast>	_padded_double;
-   //berrou<logrealfast>	_berrou_double;
-   //flat<logrealfast>   	_flat_double;
-   //helical<logrealfast>	_helical_double;
-   //rand_lut<logrealfast>	_rand_lut_double;
-   //rectangular<logrealfast>	_rectangular_double;
-   //shift_lut<logrealfast>	_shift_lut_double;
-   //uniform_lut<logrealfast>	_uniform_lut_double;
-   //named_lut<logrealfast>	_named_lut_double;
    // Modulators
    mpsk                             _mpsk;
    qam                              _qam;
-   dminner<logrealfast,false>       _dminner_logrealfast;
-   dminner<double,true>             _dminner_double;
-   dminner2<logrealfast,false>      _dminner2_logrealfast;
-   dminner2<double,true>            _dminner2_double;
-   dminner2d<logrealfast,false>     _dminner2d_logrealfast;
-   dminner2d<double,true>           _dminner2d_double;
+   dminner<double,true>             _dminner;
+   dminner2<double,true>            _dminner2;
+   dminner2d<double,true>           _dminner2d;
    // Codecs
-   repacc<libbase::logrealfast>     _repacc_logrealfast;
-   sysrepacc<libbase::logrealfast>  _sysrepacc_logrealfast;
+   repacc<double>                   _repacc;
+   sysrepacc<double>                _sysrepacc;
    // Mappers
-   map_interleaved<libbase::vector>    _map_interleaved_vector;
-   map_permuted<libbase::vector>       _map_permuted_vector;
-   map_stipple<libbase::vector>        _map_stipple_vector;
+   map_interleaved<libbase::vector>    _map_interleaved;
+   map_permuted<libbase::vector>       _map_permuted;
+   map_stipple<libbase::vector>        _map_stipple;
    // Systems
-   commsys< libbase::gf<1,0x3> >       _commsys_gf1;
-   commsys< libbase::gf<2,0x7> >       _commsys_gf2;
-   commsys< libbase::gf<3,0xB> >       _commsys_gf3;
-   commsys< libbase::gf<4,0x13> >      _commsys_gf4;
-   commsys<sigspace>                   _commsys_sigspace;
-   commsys<bool>                       _commsys_bool;
-//    commsys<bool,libbase::matrix>       _commsys_bool_matrix;
-   commsys_iterative< libbase::gf<1,0x3> >       _commsys_iterative_gf1;
-   commsys_iterative< libbase::gf<2,0x7> >       _commsys_iterative_gf2;
-   commsys_iterative< libbase::gf<3,0xB> >       _commsys_iterative_gf3;
-   commsys_iterative< libbase::gf<4,0x13> >      _commsys_iterative_gf4;
-   commsys_iterative<sigspace>                   _commsys_iterative_sigspace;
-   commsys_iterative<bool>                       _commsys_iterative_bool;
+   commsys<bool>                       _commsys;
+   commsys_iterative<bool>             _commsys_iterative;
    // Experiments
-   commsys_simulator< libbase::gf<1,0x3> >       _commsys_simulator_gf1;
-   commsys_simulator< libbase::gf<2,0x7> >       _commsys_simulator_gf2;
-   commsys_simulator< libbase::gf<3,0xB> >       _commsys_simulator_gf3;
-   commsys_simulator< libbase::gf<4,0x13> >      _commsys_simulator_gf4;
-   commsys_simulator<sigspace>                   _commsys_simulator_sigspace;
-   commsys_simulator<bool>                       _commsys_simulator_bool;
-   commsys_simulator<bool,commsys_prof_burst>    _commsys_simulator_bool_prof_burst;
-   commsys_simulator<bool,commsys_prof_pos>      _commsys_simulator_bool_prof_pos;
-   commsys_simulator<bool,commsys_prof_sym>      _commsys_simulator_bool_prof_sym;
-   commsys_simulator<bool,commsys_hist_symerr>   _commsys_simulator_bool_hist_symerr;
-   commsys_threshold<bool>             _commsys_threshold_bool;
+   commsys_simulator<bool>             _commsys_simulator;
+   commsys_threshold<bool>             _commsys_threshold;
 public:
    serializer_libcomm() :
       _mpsk(2), _qam(4) {};
