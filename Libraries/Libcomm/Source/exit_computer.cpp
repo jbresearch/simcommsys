a3ae1e7ea1912c8d91c2070022410833abce4b81
Johann A. Briffa <j.briffa@surrey.ac.uk>
2009-07-08 12:38:49 +0000

    Added placeholder for EXIT chart computation

---
+++ Libraries/Libcomm/Source/exit_computer.cpp
@@ -0,0 +1,235 @@
+/*!
+   \file
+
+   \section svn Version Control
+   - $Revision$
+   - $Date$
+   - $Author$
+*/
+
+#include "exit_computer.h"
+
+#include "map_straight.h"
+#include "fsm.h"
+#include "gf.h"
+#include "itfunc.h"
+#include "secant.h"
+#include "timer.h"
+#include <iostream>
+#include <sstream>
+
+namespace libcomm {
+
+
+// *** Templated Common Base ***
+
+// Setup functions
+
+/*!
+   \brief Sets up system with no bound objects.
+
+   \note This function is only responsible for clearing pointers to
+         objects that are specific to this object/derivation.
+         Anything else should get done automatically when the base
+         serializer or constructor is called.
+*/
+template <class S>
+void exit_computer<S>::clear()
+   {
+   src = NULL;
+   sys = NULL;
+   internallyallocated = true;
+   }
+
+/*!
+   \brief Removes association with bound objects
+
+   This function performs two things:
+   - Deletes any internally-allocated bound objects
+   - Sets up the system with no bound objects
+
+   \note This function is only responsible for deleting bound
+         objects that are specific to this object/derivation.
+         Anything else should get done automatically when the base
+         serializer or constructor is called.
+*/
+template <class S>
+void exit_computer<S>::free()
+   {
+   if(internallyallocated)
+      {
+      delete src;
+      delete sys;
+      }
+   clear();
+   }
+
+// Internal functions
+
+/*!
+   \brief Create source sequence to be encoded
+   \return Source sequence of the required length
+
+   The source sequence consists of uniformly random symbols followed by a
+   tail sequence if required by the given codec.
+*/
+template <class S>
+libbase::vector<int> exit_computer<S>::createsource()
+   {
+   const int tau = sys->getcodec()->input_block_size();
+   libbase::vector<int> source(tau);
+   for(int t=0; t<tau; t++)
+      source(t) = src->ival(get_alphabetsize());
+   return source;
+   }
+
+/*!
+   \brief Perform a complete encode->transmit->receive cycle
+   \param[out] result   Vector containing the set of results to be updated
+
+   Results are organized as (BER,SER,FER), repeated for every iteration that
+   needs to be performed.
+
+   \note It is assumed that the result vector serves as an accumulator, so that
+         every cycle effectively adds to this result. The caller is responsible
+         to divide by the appropriate amount at the end to compute a meaningful
+         average.
+*/
+template <class S>
+void exit_computer<S>::cycleonce(libbase::vector<double>& result)
+   {
+   assert(result.size() == count());
+   // Create source stream
+   libbase::vector<int> source = createsource();
+   // Full cycle from Encode through Demodulate
+   sys->transmitandreceive(source);
+   // For every iteration
+   libbase::vector<int> decoded;
+   for(int i=0; i<get_iter(); i++)
+      {
+      // Decode & update results
+      sys->getcodec()->decode(decoded);
+      //R::updateresults(result, i, source, decoded);
+      }
+   // Keep record of what we last simulated
+   const int tau = sys->getcodec()->input_block_size();
+   assert(source.size() == tau);
+   assert(decoded.size() == tau);
+   last_event.init(2*tau);
+   for(int i=0; i<tau; i++)
+      {
+      last_event(i) = source(i);
+      last_event(i+tau) = decoded(i);
+      }
+   }
+
+// Constructors / Destructors
+
+/*!
+   \brief Main public constructor
+
+   Initializes system with bound objects as supplied by user.
+*/
+template <class S>
+exit_computer<S>::exit_computer(libbase::randgen *src, commsys<S> *sys)
+   {
+   this->src = src;
+   this->sys = sys;
+   internallyallocated = false;
+   }
+
+/*!
+   \brief Copy constructor
+
+   Initializes system with bound objects cloned from supplied system.
+*/
+template <class S>
+exit_computer<S>::exit_computer(const exit_computer<S>& c)
+   {
+   this->src = new libbase::randgen;
+   this->sys = c.sys->clone();
+   internallyallocated = true;
+   }
+
+// Experiment parameter handling
+
+template <class S>
+void exit_computer<S>::seedfrom(libbase::random& r)
+   {
+   src->seed(r.ival());
+   sys->seedfrom(r);
+   }
+
+// Experiment handling
+
+template <class S>
+void exit_computer<S>::sample(libbase::vector<double>& result)
+   {
+   // initialise result vector
+   result.init(count());
+   result = 0;
+   // compute a single cycle
+   cycleonce(result);
+   }
+
+// Description & Serialization
+
+template <class S>
+std::string exit_computer<S>::description() const
+   {
+   std::ostringstream sout;
+   sout << "Simulator for ";
+   sout << sys->description();
+   return sout.str();
+   }
+
+template <class S>
+std::ostream& exit_computer<S>::serialize(std::ostream& sout) const
+   {
+   sout << sys;
+   return sout;
+   }
+
+template <class S>
+std::istream& exit_computer<S>::serialize(std::istream& sin)
+   {
+   free();
+   src = new libbase::randgen;
+   sin >> sys;
+   internallyallocated = true;
+   return sin;
+   }
+
+// Explicit Realizations
+
+using libbase::serializer;
+using libbase::gf;
+
+template class exit_computer<sigspace>;
+template <>
+const serializer exit_computer<sigspace>::shelper("experiment", "exit_computer<sigspace>", exit_computer<sigspace>::create);
+
+template class exit_computer<bool>;
+template <>
+const serializer exit_computer<bool>::shelper("experiment", "exit_computer<bool>", exit_computer<bool>::create);
+
+template class exit_computer< gf<1,0x3> >;
+template <>
+const serializer exit_computer< gf<1,0x3> >::shelper("experiment", "exit_computer<gf<1,0x3>>", exit_computer< gf<1,0x3> >::create);
+template class exit_computer< gf<2,0x7> >;
+template <>
+const serializer exit_computer< gf<2,0x7> >::shelper("experiment", "exit_computer<gf<2,0x7>>", exit_computer< gf<2,0x7> >::create);
+template class exit_computer< gf<3,0xB> >;
+template <>
+const serializer exit_computer< gf<3,0xB> >::shelper("experiment", "exit_computer<gf<3,0xB>>", exit_computer< gf<3,0xB> >::create);
+template class exit_computer< gf<4,0x13> >;
+template <>
+const serializer exit_computer< gf<4,0x13> >::shelper("experiment", "exit_computer<gf<4,0x13>>", exit_computer< gf<4,0x13> >::create);
+
+// realizations for non-default containers
+
+// template class exit_computer<bool,matrix>;
+// template <>
+// const serializer exit_computer<bool,matrix>::shelper("experiment", "exit_computer<bool,matrix>", exit_computer<bool,matrix>::create);
+
+}; // end namespace
---
+++ Libraries/Libcomm/Source/exit_computer.h
@@ -0,0 +1,85 @@
+#ifndef __exit_computer_h
+#define __exit_computer_h
+
+#include "config.h"
+#include "experiment_normal.h"
+#include "commsys.h"
+#include "randgen.h"
+#include "serializer.h"
+
+namespace libcomm {
+
+/*!
+   \brief   EXIT Chart Computer.
+   \author  Johann Briffa
+
+   \section svn Version Control
+   - $Revision$
+   - $Date$
+   - $Author$
+
+   \warning Currently this is just a placeholder; functionality still needs
+            to be written.
+*/
+
+template <class S>
+class exit_computer
+   : public experiment_normal {
+protected:
+   /*! \name Bound objects */
+   //! Flag to indicate whether the objects should be released on destruction
+   bool  internallyallocated;
+   libbase::randgen     *src;    //!< Source data sequence generator
+   commsys<S>           *sys;    //!< Communication systems
+   // @}
+   /*! \name Internal state */
+   libbase::vector<int> last_event;
+   // @}
+protected:
+   /*! \name Setup functions */
+   void clear();
+   void free();
+   // @}
+   /*! \name Internal functions */
+   libbase::vector<int> createsource();
+   void cycleonce(libbase::vector<double>& result);
+   // @}
+   // System Interface for Results
+   int get_iter() const { return sys->getcodec()->num_iter(); };
+   int get_symbolsperblock() const { return sys->getcodec()->input_block_size(); };
+   int get_alphabetsize() const { return sys->getcodec()->num_inputs(); };
+public:
+   /*! \name Constructors / Destructors */
+   exit_computer(libbase::randgen *src, commsys<S> *sys);
+   exit_computer(const exit_computer<S>& c);
+   exit_computer() { clear(); };
+   virtual ~exit_computer() { free(); };
+   // @}
+
+   // Experiment parameter handling
+   void seedfrom(libbase::random& r);
+   void set_parameter(const double x) { sys->getchan()->set_parameter(x); };
+   double get_parameter() const { return sys->getchan()->get_parameter(); };
+
+   // Experiment handling
+   void sample(libbase::vector<double>& result);
+   int count() const { return 1; };
+   int get_multiplicity(int i) const { return 1; };
+   std::string result_description(int i) const { return ""; };
+   libbase::vector<int> get_event() const { return last_event; };
+
+   /*! \name Component object handles */
+   //! Get communication system
+   const commsys<S> *getsystem() const { return sys; };
+   // @}
+
+   // Description
+   std::string description() const;
+
+   // Serialization Support
+   DECLARE_SERIALIZER(exit_computer);
+};
+
+}; // end namespace
+
+#endif
