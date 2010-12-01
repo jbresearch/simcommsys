dba3d779ed54e1605e582d8c8c998996881fc4bc
Johann A. Briffa <j.briffa@surrey.ac.uk>
2010-12-01 18:13:20 +0000

    Prepended license text to all source files

--- Libraries/Libwin/StringStreamBuf.cpp
+++ Libraries/Libwin/StringStreamBuf.cpp
@@ -1,3 +1,27 @@
+/*!
+ * \file
+ * 
+ * Copyright (c) 2010 Johann A. Briffa
+ * 
+ * This file is part of SimCommSys.
+ *
+ * SimCommSys is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * SimCommSys is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with SimCommSys.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * \section svn Version Control
+ * - $Id$
+ */
+
 #include "stdafx.h"
 #include "StringStreamBuf.h"
 
