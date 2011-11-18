6ee02f4efd0db549614038afad7d9b78067ca655
Johann A. Briffa <j.briffa@surrey.ac.uk>
2011-11-18 13:33:37 +0000

    Reorganization

--- Test/cudacomparisontest.sh
+++ Test/cudacomparisontest.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 #$Id$
 
-make
+# system file and simulation parameters
 file="Simulators/bsid-dminner2_4_8_h-uncoded-gf16-50-float.txt"
-tag=`dirname $PWD`
-tag=`basename $tag`
+par="1e-2"
+seed="0"
+# branch name
+branch=`dirname $PWD`
+branch=`basename $branch`
+
+make
+
+tag="$branch"
 echo "Running test for build $tag"
-showerrorevent.$tag.debug -p 1e-2 -s 0 -i $file >$tag.log 2>&1
-tag="$tag-mpi-gmp-cuda11"
+showerrorevent.$tag.debug -p $par -s $seed -i $file >$tag.log 2>&1
+
+tag="$branch-mpi-gmp-cuda11"
 echo "Running test for build $tag"
-showerrorevent.$tag.debug -p 1e-2 -s 0 -i $file >$tag.log 2>&1
+showerrorevent.$tag.debug -p $par -s $seed -i $file >$tag.log 2>&1
