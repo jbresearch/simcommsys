a03d47b924957308fc5e0ef817c947379a8f5b98
Johann A. Briffa <j.briffa@surrey.ac.uk>
2013-08-28 15:54:34 +0000

    Updated script for setting folder ignore properties

--- Scripts/setignores.sh
+++ Scripts/setignores.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 #$Id$
 
+# root-level folder
+folder="."
+echo "*** $folder"
+propval=[Dd]ebug$'\n'[Rr]elease$'\n'[Pp]rofile$'\n'Win32$'\n'x64
+svn ps svn:ignore "$propval" $folder
+#svn pg svn:ignore $folder
 # top-level folders for libraries and targets
 for folder in Libraries SimCommsys Steganography Test Windows; do
    echo "*** $folder"
@@ -10,7 +16,7 @@ for folder in Libraries SimCommsys Steganography Test Windows; do
    # individual folders for libraries and targets
    for target in $folder/*; do
       echo "*** $target"
-      propval=[Dd]ebug$'\n'[Rr]elease$'\n'[Pp]rofile$'\n'*.s$'\n'*.ii$'\n'*.vcxproj.user
+      propval=[Dd]ebug$'\n'[Rr]elease$'\n'[Pp]rofile$'\n'*.s$'\n'*.ii$'\n'Win32$'\n'x64$'\n'*.vcxproj.user
       svn ps svn:ignore "$propval" $target
       #svn pg svn:ignore $target
    done
