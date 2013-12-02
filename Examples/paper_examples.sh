4b140d11b31f70181593e2cd3f282e94a5c85435
Johann A. Briffa <j.briffa@surrey.ac.uk>
2013-12-02 17:03:28 +0000

    Added script to run quick sims for paper Also added folder to hold results

---
+++ Examples/paper_examples.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#$Id$
+
+# first make sure we have all systems set up
+make all
+
+echo "Quick simulation with 'uncoded' codec..."
+quicksimulation.master.release -t 10 -r 6.8 -i Simulators/errors_hamming-random-awgn-bpsk-uncoded.txt >Results/sim.errors_hamming-random-awgn-bpsk-uncoded.txt 2>/dev/null
+
+echo "Quick simulation with concatenated RS-convolutional codecs..."
+quicksimulation.master.release -t 10 -r 2.2 -i Simulators/errors_hamming-random-awgn-bpsk-concantenated-reedsolomon_255_223_gf256-map_interleaved-nrcc_133_171.txt >Results/sim.errors_hamming-random-awgn-bpsk-concantenated-reedsolomon_255_223_gf256-map_interleaved-nrcc_133_171.txt 2>/dev/null
