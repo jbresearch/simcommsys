2d625eda540dac3562b6ea8920bd1a01561b9226
Johann A. Briffa <j.briffa@surrey.ac.uk>
2013-12-02 14:00:08 +0000

    Updated script and set ignore properties

--- Scripts/setignores.sh
+++ Scripts/setignores.sh
@@ -5,19 +5,16 @@
 folder="."
 echo "*** $folder"
 propval=doc$'\n'[Dd]ebug$'\n'[Rr]elease$'\n'[Pp]rofile$'\n'Win32$'\n'x64$'\n'*.suo$'\n'*.ncb
-svn ps svn:ignore "$propval" $folder
-#svn pg svn:ignore $folder
+echo "$propval" > $folder/.gitignore
 # top-level folders for libraries and targets
 for folder in Libraries SimCommsys Steganography Test Windows; do
    echo "*** $folder"
    propval=[Dd]ebug$'\n'[Rr]elease$'\n'[Pp]rofile$'\n'*.suo$'\n'*.ncb$'\n'*cache.dat$'\n'*.aps
-   svn ps svn:ignore "$propval" $folder
-   #svn pg svn:ignore $folder
+   echo "$propval" > $folder/.gitignore
    # individual folders for libraries and targets
    for target in $folder/*; do
       echo "*** $target"
       propval=[Dd]ebug$'\n'[Rr]elease$'\n'[Pp]rofile$'\n'*.s$'\n'*.ii$'\n'Win32$'\n'x64$'\n'*.vcxproj.user
-      svn ps svn:ignore "$propval" $target
-      #svn pg svn:ignore $target
+      echo "$propval" > $target/.gitignore
    done
 done
