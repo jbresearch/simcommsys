4ab8dafce25d13d67858d1ee77c60ce36f2ab825
Johann A. Briffa <j.briffa@surrey.ac.uk>
2014-08-06 13:21:46 +0100

    Added memory limit to local submitter

--- Scripts/Clients/Local/submit-local.sh
+++ Scripts/Clients/Local/submit-local.sh
@@ -6,6 +6,14 @@ if [[ $# < 1 ]]; then
    exit
 fi
 
+# set memory limit
+if [ -f /proc/meminfo -a -f /proc/cpuinfo -a -e $(which gawk) ]; then
+   CPUS=$(gawk '/processor/ { n++ } END { print n }' /proc/cpuinfo)
+   vlimit=$(gawk -v n="$CPUS" '/MemTotal/ { printf "%d",$2/n }' /proc/meminfo)
+   echo "Setting memory limit to $vlimit KiB"
+   ulimit -v $vlimit
+fi
+
 # determine main parameters
 executable=`which $1`
 shift 1
