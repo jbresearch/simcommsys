6d43b7868760f18344999ab9b707e605d5357c4c
Johann A. Briffa <johann.briffa@um.edu.mt>
2018-02-05 18:19:01 +0100

    Created all-zero source type

---
+++ Libraries/Libcomm/source/all_zero.cpp
@@ -0,0 +1,87 @@
+/*!
+ * \file
+ *
+ * Copyright (c) 2010 Johann A. Briffa
+ *
+ * This file is part of SimCommSys.
+ *
+ * SimCommSys is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * SimCommSys is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with SimCommSys.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "all_zero.h"
+#include <sstream>
+
+namespace libcomm {
+
+// object serialization - saving
+
+template <class S, template <class > class C>
+std::ostream& all_zero<S, C>::serialize(std::ostream& sout) const
+   {
+   return sout;
+   }
+
+// object serialization - loading
+
+template <class S, template <class > class C>
+std::istream& all_zero<S, C>::serialize(std::istream& sin)
+   {
+   return sin;
+   }
+
+} // end namespace
+
+#include "gf.h"
+
+namespace libcomm {
+
+// Explicit Realizations
+#include <boost/preprocessor/seq/for_each.hpp>
+#include <boost/preprocessor/seq/for_each_product.hpp>
+#include <boost/preprocessor/seq/enum.hpp>
+#include <boost/preprocessor/stringize.hpp>
+
+using libbase::serializer;
+using libbase::matrix;
+using libbase::vector;
+
+#define USING_GF(r, x, type) \
+      using libbase::type;
+
+BOOST_PP_SEQ_FOR_EACH(USING_GF, x, GF_TYPE_SEQ)
+
+#define SYMBOL_TYPE_SEQ \
+   (int) \
+   GF_TYPE_SEQ
+#define CONTAINER_TYPE_SEQ \
+   (vector)
+   //(vector)(matrix)
+
+/* Serialization string: all_zero<type,container>
+ * where:
+ *      type = int | gf2 | gf4 ...
+ *      container = vector | matrix
+ */
+#define INSTANTIATE(r, args) \
+      template class all_zero<BOOST_PP_SEQ_ENUM(args)>; \
+      template <> \
+      const serializer all_zero<BOOST_PP_SEQ_ENUM(args)>::shelper( \
+            "source", \
+            "all_zero<" BOOST_PP_STRINGIZE(BOOST_PP_SEQ_ELEM(0,args)) "," \
+            BOOST_PP_STRINGIZE(BOOST_PP_SEQ_ELEM(1,args)) ">", \
+            all_zero<BOOST_PP_SEQ_ENUM(args)>::create);
+
+BOOST_PP_SEQ_FOR_EACH_PRODUCT(INSTANTIATE, (SYMBOL_TYPE_SEQ)(CONTAINER_TYPE_SEQ))
+
+} // end namespace
---
+++ Libraries/Libcomm/source/all_zero.h
@@ -0,0 +1,60 @@
+/*!
+ * \file
+ *
+ * Copyright (c) 2010 Johann A. Briffa
+ *
+ * This file is part of SimCommSys.
+ *
+ * SimCommSys is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * SimCommSys is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with SimCommSys.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __all_zero_h
+#define __all_zero_h
+
+#include "config.h"
+#include "source.h"
+#include "serializer.h"
+
+namespace libcomm {
+
+/*!
+ * \brief   All-zero source.
+ * \author  Johann Briffa
+ *
+ * Implements a trivial source that always returns the zero symbol.
+ */
+
+template <class S, template <class > class C = libbase::vector>
+class all_zero : public source<S, C> {
+public:
+   //! Generate a single source element
+   S generate_single()
+      {
+      return 0;
+      }
+
+   //! Description
+   std::string description() const
+      {
+      return "All-zero source";
+      }
+
+   // Serialization Support
+DECLARE_SERIALIZER(all_zero)
+};
+
+} // end namespace
+
+#endif
+
