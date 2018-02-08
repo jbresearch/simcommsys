fef1934742ec3b105e8312084b460c8687c25157
Johann A. Briffa <johann.briffa@um.edu.mt>
2018-02-08 12:39:42 +0100

    Uniform random source

---
+++ Libraries/Libcomm/source/uniform.cpp
@@ -0,0 +1,112 @@
+/*!
+ * \file
+ *
+ * Copyright (c) 2010 Johann A. Briffa
+ *
+ * This file is part of SimCommSys.
+ *
+ * SimCommSys is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * SimCommSys is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with SimCommSys.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "uniform.h"
+#include <sstream>
+
+namespace libcomm {
+
+// *** Base Class ***
+
+// object serialization - saving
+
+template <class S, template <class > class C>
+std::ostream& uniform<S, C>::serialize(std::ostream& sout) const
+   {
+   return sout;
+   }
+
+// object serialization - loading
+
+template <class S, template <class > class C>
+std::istream& uniform<S, C>::serialize(std::istream& sin)
+   {
+   return sin;
+   }
+
+// *** int specialization ***
+
+// object serialization - saving
+
+template <template <class > class C>
+std::ostream& uniform<int, C>::serialize(std::ostream& sout) const
+   {
+   sout << "# Alphabet size" << std::endl;
+   sout << alphabet_size << std::endl;
+   return sout;
+   }
+
+// object serialization - loading
+
+template <template <class > class C>
+std::istream& uniform<int, C>::serialize(std::istream& sin)
+   {
+   assertalways(sin.good());
+   // get alphabet size
+   sin >> libbase::eatcomments >> alphabet_size;
+   return sin;
+   }
+
+} // end namespace
+
+#include "gf.h"
+
+namespace libcomm {
+
+// Explicit Realizations
+#include <boost/preprocessor/seq/for_each.hpp>
+#include <boost/preprocessor/seq/for_each_product.hpp>
+#include <boost/preprocessor/seq/enum.hpp>
+#include <boost/preprocessor/stringize.hpp>
+
+using libbase::serializer;
+using libbase::matrix;
+using libbase::vector;
+
+#define USING_GF(r, x, type) \
+      using libbase::type;
+
+BOOST_PP_SEQ_FOR_EACH(USING_GF, x, GF_TYPE_SEQ)
+
+#define SYMBOL_TYPE_SEQ \
+   (int) \
+   GF_TYPE_SEQ
+#define CONTAINER_TYPE_SEQ \
+   (vector)
+   //(vector)(matrix)
+
+/* Serialization string: uniform<type,container>
+ * where:
+ *      type = int | gf2 | gf4 ...
+ *      container = vector | matrix
+ */
+#define INSTANTIATE(r, args) \
+      template class uniform<BOOST_PP_SEQ_ENUM(args)>; \
+      template <> \
+      const serializer uniform<BOOST_PP_SEQ_ENUM(args)>::shelper( \
+            "source", \
+            "uniform<" BOOST_PP_STRINGIZE(BOOST_PP_SEQ_ELEM(0,args)) "," \
+            BOOST_PP_STRINGIZE(BOOST_PP_SEQ_ELEM(1,args)) ">", \
+            uniform<BOOST_PP_SEQ_ENUM(args)>::create);
+
+BOOST_PP_SEQ_FOR_EACH_PRODUCT(INSTANTIATE, (SYMBOL_TYPE_SEQ)(CONTAINER_TYPE_SEQ))
+
+} // end namespace
---
+++ Libraries/Libcomm/source/uniform.h
@@ -0,0 +1,111 @@
+/*!
+ * \file
+ *
+ * Copyright (c) 2010 Johann A. Briffa
+ *
+ * This file is part of SimCommSys.
+ *
+ * SimCommSys is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * SimCommSys is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with SimCommSys.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __source_uniform_h
+#define __source_uniform_h
+
+#include "config.h"
+#include "source.h"
+#include "serializer.h"
+#include "field_utils.h"
+
+namespace libcomm {
+
+/*!
+ * \brief   Uniform random source.
+ * \author  Johann Briffa
+ *
+ * Implements a source that returns a uniformly-distributed random symbol
+ * from the given alphabet.
+ */
+
+template <class S, template <class > class C = libbase::vector>
+class uniform : public source<S, C> {
+private:
+   /*! \name Internal representation */
+   libbase::randgen r; //!< Data sequence generator
+   // @}
+public:
+   //! Generate a single source element
+   S generate_single()
+      {
+      return r.ival(field_utils<S>::elements());
+      }
+
+   //! Seeds any random generators from a pseudo-random sequence
+   void seedfrom(libbase::random& r)
+      {
+      this->r.seed(r.ival());
+      }
+
+   //! Description
+   std::string description() const
+      {
+      return "Uniform random source";
+      }
+
+   // Serialization Support
+DECLARE_SERIALIZER(uniform)
+};
+
+/*!
+ * \brief   Uniform random source specialisation.
+ * \author  Johann Briffa
+ *
+ * Implements a source that returns a uniformly-distributed random symbol
+ * from the given alphabet. Partial specialisation for int (needs to obtain
+ * alphabet size from serialization).
+ */
+
+template <template <class > class C>
+class uniform<int, C> : public source<int, C> {
+private:
+   /*! \name Internal representation */
+   libbase::randgen r; //!< Data sequence generator
+   int alphabet_size; //!< Number of elements in alphabet
+   // @}
+public:
+   //! Generate a single source element
+   int generate_single()
+      {
+      return r.ival(alphabet_size);
+      }
+
+   //! Seeds any random generators from a pseudo-random sequence
+   void seedfrom(libbase::random& r)
+      {
+      this->r.seed(r.ival());
+      }
+
+   //! Description
+   std::string description() const
+      {
+      return "Uniform random source";
+      }
+
+   // Serialization Support
+DECLARE_SERIALIZER(uniform)
+};
+
+} // end namespace
+
+#endif
+
