d12fef3f5a18d336eed585188912f95421f71ea1
Johann A. Briffa <johann.briffa@um.edu.mt>
2018-02-08 12:53:22 +0100

    Sequential source

---
+++ Libraries/Libcomm/source/sequential.cpp
@@ -0,0 +1,107 @@
+/*!
+ * \file
+ *
+ * Copyright (c) 2010 Johann A. Briffa
+ *
+ * This file is part of SimCommSys.
+ *
+ * SimCommSys is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * SimCommSys is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with SimCommSys.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "sequential.h"
+#include <sstream>
+
+namespace libcomm {
+
+// object serialization - saving
+
+template <class S, template <class > class C>
+std::ostream& sequential<S, C>::serialize(std::ostream& sout) const
+   {
+   sout << "# Version" << std::endl;
+   sout << 1 << std::endl;
+   sout << "#: input symbols - count" << std::endl;
+   sout << input_vectors.size() << std::endl;
+   sout << "#: input symbols - values" << std::endl;
+   input_vectors.serialize(sout, '\n');
+   return sout;
+   }
+
+// object serialization - loading
+
+template <class S, template <class > class C>
+std::istream& sequential<S, C>::serialize(std::istream& sin)
+   {
+   assertalways(sin.good());
+   // get format version
+   int version;
+   sin >> libbase::eatcomments >> version;
+   // read count of input symbols
+   int temp;
+   sin >> libbase::eatcomments >> temp >> libbase::verify;
+   // read input symbols from stream
+   input_vectors.init(temp);
+   sin >> libbase::eatcomments;
+   input_vectors.serialize(sin);
+   libbase::verify(sin);
+   // reset index
+   index = 0;
+   return sin;
+   }
+
+} // end namespace
+
+#include "gf.h"
+
+namespace libcomm {
+
+// Explicit Realizations
+#include <boost/preprocessor/seq/for_each.hpp>
+#include <boost/preprocessor/seq/for_each_product.hpp>
+#include <boost/preprocessor/seq/enum.hpp>
+#include <boost/preprocessor/stringize.hpp>
+
+using libbase::serializer;
+using libbase::matrix;
+using libbase::vector;
+
+#define USING_GF(r, x, type) \
+      using libbase::type;
+
+BOOST_PP_SEQ_FOR_EACH(USING_GF, x, GF_TYPE_SEQ)
+
+#define SYMBOL_TYPE_SEQ \
+   (int) \
+   GF_TYPE_SEQ
+#define CONTAINER_TYPE_SEQ \
+   (vector)
+   //(vector)(matrix)
+
+/* Serialization string: sequential<type,container>
+ * where:
+ *      type = int | gf2 | gf4 ...
+ *      container = vector | matrix
+ */
+#define INSTANTIATE(r, args) \
+      template class sequential<BOOST_PP_SEQ_ENUM(args)>; \
+      template <> \
+      const serializer sequential<BOOST_PP_SEQ_ENUM(args)>::shelper( \
+            "source", \
+            "sequential<" BOOST_PP_STRINGIZE(BOOST_PP_SEQ_ELEM(0,args)) "," \
+            BOOST_PP_STRINGIZE(BOOST_PP_SEQ_ELEM(1,args)) ">", \
+            sequential<BOOST_PP_SEQ_ENUM(args)>::create);
+
+BOOST_PP_SEQ_FOR_EACH_PRODUCT(INSTANTIATE, (SYMBOL_TYPE_SEQ)(CONTAINER_TYPE_SEQ))
+
+} // end namespace
---
+++ Libraries/Libcomm/source/sequential.h
@@ -0,0 +1,70 @@
+/*!
+ * \file
+ *
+ * Copyright (c) 2010 Johann A. Briffa
+ *
+ * This file is part of SimCommSys.
+ *
+ * SimCommSys is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * SimCommSys is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with SimCommSys.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __source_sequential_h
+#define __source_sequential_h
+
+#include "config.h"
+#include "source.h"
+#include "serializer.h"
+#include <sstream>
+
+namespace libcomm {
+
+/*!
+ * \brief   All-sequential source.
+ * \author  Johann Briffa
+ *
+ * Implements a trivial source that always returns the sequential symbol.
+ */
+
+template <class S, template <class > class C = libbase::vector>
+class sequential : public source<S, C> {
+private:
+   /*! \name Internal representation */
+   libbase::vector<S> input_vectors; //!< user sequence of input symbols
+   int index; //!< index of next element to output
+   // @}
+public:
+   //! Generate a single source element
+   S generate_single()
+      {
+      S value = input_vectors(index);
+      index = (index + 1) % input_vectors.size();
+      return value;
+      }
+
+   //! Description
+   std::string description() const
+      {
+      std::ostringstream sout;
+      sout << "Sequential source [" << input_vectors.size() << " elements]";
+      return sout.str();
+      }
+
+   // Serialization Support
+DECLARE_SERIALIZER(sequential)
+};
+
+} // end namespace
+
+#endif
+
