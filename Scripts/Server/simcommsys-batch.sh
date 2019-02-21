327ce65da97d10fcf90cf7d6c270d3fa81906856
Johann A. Briffa <johann.briffa@um.edu.mt>
2019-02-21 16:18:52 +0100

    Require specification of vlimit

--- Scripts/Server/simcommsys-batch.sh
+++ Scripts/Server/simcommsys-batch.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 if (( $# < 4 )); then
-   echo "Usage: ${0##*/} <tag> <port> <other> [<systems>]"
+   echo "Usage: ${0##*/} <tag> <port> <vlimit> <other> [<systems>]"
    echo "<tag> is the build tag name (XX in simcommsys.XX.release)"
    echo "<port> is the first port to use, then decrement (or 'local')"
+   echo "<vlimit> is the virtual memory limit in KiB (or 'default' or 'unlimited')"
+   echo "   default = divide available memory by number of (virtual) CPUs"
    echo "<other> contains any other parameters to pass to simcommsys"
    echo "        (may be blank but must be present)"
    echo "<systems> is a list of systems to simulate in parallel"
@@ -21,8 +23,9 @@ fi
 path=${0%/*}
 tag=$1
 port=$2
-other=$3
-shift 3
+vlimit=$3
+other=$4
+shift 5
 # interpret port numbering
 if [[ $port != "local" ]]; then declare -i port; fi
 
@@ -35,7 +38,7 @@ while (( $# > 0 )); do
    echo "  Port:   $port"
    echo "  Args:   $other"
 
-   screen -d -m -S "$port.$system" "$path/simcommsys-wrapper.sh" "$systempath" "$tag" "$port" "default" $other
+   screen -d -m -S "$port.$system" "$path/simcommsys-wrapper.sh" "$systempath" "$tag" "$port" "$vlimit" $other
    # interpret port numbering
    if [[ $port != "local" ]]; then (( port -- )); fi
    shift
