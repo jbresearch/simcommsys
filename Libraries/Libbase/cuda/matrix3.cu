f450d494c9fb2d1c8c02c1d473aa738d0375a824
Mark Mizzi <mark.mizzi.19@um.edu.mt>
2024-03-26 16:01:05 +0100

    add explicit instantiations for matrix3

---
+++ Libraries/Libbase/cuda/matrix3.cu
@@ -0,0 +1,56 @@
+/*!
+ * \file
+ *
+ * Copyright (c) 2024 Mark Mizzi
+ *
+ * This file is part of SimCommSys.
+ *
+ * SimCommSys is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * SimCommSys is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with SimCommSys.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*!
+ * \file
+ * \brief   A three-dimensional array in device memory.
+ * \author  Mark Mizzi
+ */
+
+#include "cuda-all.h"
+#include "gf.h"
+
+namespace cuda
+{
+
+// Explicit Realizations
+#include <boost/preprocessor/seq/for_each.hpp>
+#include <boost/preprocessor/stringize.hpp>
+
+#define USING_GF(r, x, type) using libbase::type;
+
+BOOST_PP_SEQ_FOR_EACH(USING_GF, x, GF_TYPE_SEQ)
+
+// clang-format off
+#define TYPE_SEQ \
+   (bool)(int)(float)(double) \
+   GF_TYPE_SEQ
+// clang-format on
+
+#define INSTANTIATE(r, x, type)                                                \
+    template class matrix3<type>;                                              \
+    template class matrix3<vector<type>>;                                      \
+    template class matrix3<vector_reference<type>>;                            \
+    template class matrix3<vector_auto<type>>;
+
+BOOST_PP_SEQ_FOR_EACH(INSTANTIATE, x, TYPE_SEQ)
+
+} // namespace cuda
