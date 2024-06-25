6b2b22728b65c59bc4f41ae295f9059f2fc222a3
Mark Mizzi <mark.mizzi@um.edu.mt>
2024-06-25 20:27:28 +0200

    use make_shared in place of direct shared_ptr constructor

--- Libraries/Libcomm/sumprodalg/spa_factory.h
+++ Libraries/Libcomm/sumprodalg/spa_factory.h
@@ -65,15 +65,15 @@ public:
     {
         std::shared_ptr<sum_prod_alg_inf<GF_q, real>> spa_ptr;
         if ("trad" == type) {
-            spa_ptr = std::make_shared<sum_prod_alg_inf<GF_q, real>>(
+            spa_ptr = std::make_shared<sum_prod_alg_trad<GF_q, real>>(
                 n, m, non_zero_col_pos, non_zero_row_pos, pchk_matrix);
         } else if ("gdl" == type) {
-            spa_ptr = std::make_shared<sum_prod_alg_inf<GF_q, real>>(
+            spa_ptr = std::make_shared<sum_prod_alg_gdl<GF_q, real>>(
                 n, m, non_zero_col_pos, non_zero_row_pos, pchk_matrix);
         }
 #ifdef USE_CUDA
         else if ("gdl_cuda" == type) {
-            spa_ptr = std::make_shared<sum_prod_alg_inf<GF_q, real>>(
+            spa_ptr = std::make_shared<sum_prod_alg_gdl_cuda<GF_q, real>>(
                 n, m, non_zero_col_pos, non_zero_row_pos, pchk_matrix);
         }
 #endif
