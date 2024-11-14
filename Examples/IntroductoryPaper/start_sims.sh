49975d2d2f5f8ac58d3724a21d5b5b0963c37419
Mark Mizzi <mark.mizzi@um.edu.mt>
2024-11-14 14:32:17 +0100

    start updating IntroductoryPaper example to use new simcommsys-utils

--- Examples/IntroductoryPaper/start_sims.sh
+++ Examples/IntroductoryPaper/start_sims.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 
-settings="--start 1.5 --stop 11 --step 0.1 --floor-min 1e-5"
-settings="$settings --confidence 0.95 --relative-error 0.05"
+set -e
 
-#simcommsys-batch.sh jab local "$settings" Simulators/*
-
-#Simulators/errors_hamming-random-awgn-bpsk-concantenated-reedsolomon_255_223_gf256-map_interleaved-nrcc_133_171.txt
-#Simulators/errors_hamming-random-awgn-bpsk-nrcc_133_171.txt
-#Simulators/errors_hamming-random-awgn-bpsk-uncoded.txt
-
-simcommsys-batch.sh jab 9999 "$settings" \
-   Simulators/errors_hamming-random-awgn-bpsk-concantenated-reedsolomon_255_223_gf256-map_interleaved-nrcc_133_171.txt \
-   Simulators/errors_hamming-random-awgn-bpsk-nrcc_133_171.txt
+cd ../../Scripts/SimcommsysUtils
+poetry run simcommsys-utils run-jobs --config-file \
+   ../../Examples/IntroductoryPaper/Configurations/local-jobs.yaml
