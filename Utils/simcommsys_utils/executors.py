49c0d621f403fbc87f5c413591947a75186a9087
Mark Mizzi <mark.mizzi@um.edu.mt>
2024-11-18 09:52:35 +0100

    refactor executor interface

--- Utils/simcommsys_utils/executors.py
+++ Utils/simcommsys_utils/executors.py
@@ -20,6 +20,7 @@ import subprocess
 from dataclasses import dataclass
 from typing import Literal
 import logging
+from multiprocessing import cpu_count
 
 
 @dataclass
@@ -37,23 +38,16 @@ class SimcommsysJob:
 
 
 class SimcommsysExecutor(abc.ABC):
-    @abc.abstractmethod
-    def _get_exec_cmd(
-        self,
-        jobname: str,
-        cmd: str,
-        **jobdetails,
-    ) -> str:
-        """
-        Returns command used to execute a single simcommsys simulation on the remote.
-        """
-        raise NotImplementedError
-
     def _get_simcommsys_cmd(
-        self, tag: str, type: Literal["debug", "release"], job: SimcommsysJob
+        self,
+        simcommsys_tag: str,
+        simcommsys_type: Literal["debug", "release"],
+        job: SimcommsysJob,
     ):
         """
         Returns simcommsys command for a single simulation.
+
+        NOTE that the -e flag is not given and must be provided by a subclass
         """
         assert job.step or job.mul, "Must specify step or mul in SimmcommsysJob object."
         step_or_mul_opt: str
@@ -62,18 +56,18 @@ class SimcommsysExecutor(abc.ABC):
         else:
             step_or_mul_opt = f"--mul={job.mul}"
 
-        return f"simcommsys.{tag}.{type} \
+        return f"simcommsys.{simcommsys_tag}.{simcommsys_type} \
                     -i {job.inputfile} -o {job.outputfile} \
                     --start {job.start} --stop {job.stop} {step_or_mul_opt} \
                     --confidence {job.confidence} --relative-error {job.relative_error} \
                     --floor-min {job.floor_min} \
-                    -e local -f json"
+                    -f json"
 
     @abc.abstractmethod
     def run(
         self,
-        tag: str,
-        type: Literal["debug", "release"],
+        simcommsys_tag: str,
+        simcommsys_type: Literal["debug", "release"],
         jobs: list[SimcommsysJob],
         **jobparams,
     ):
@@ -98,40 +92,10 @@ class SlurmSimcommsysExecutor(SimcommsysExecutor):
         self.cpu_partition = cpu_partition
         self.account = account
 
-    def _get_exec_cmd(  # type:ignore
-        self,
-        jobname: str,
-        cmd: str,
-        *,
-        needs_gpu: bool,
-        n_gpus: int,
-        gpuarch: str,
-        memlimit_gb: int,
-        timeout_mins: int,
-        nodename: str | None,
-    ) -> str:
-        sbatch_opts = f"--mail-user={self.email} \
-                        --job-name={jobname} \
-                        --partition={self.gpu_partition if needs_gpu else self.cpu_partition} \
-                        --ntasks=1 \
-                        --cpus-per-task=1 \
-                        --mem-per-cpu={memlimit_gb}G \
-                        --time={timeout_mins} \
-                        --output={jobname}.out \
-                        --error={jobname}.err \
-                        --account={self.account} \
-                        --mail-type=all"
-        if needs_gpu:
-            sbatch_opts += f" --gres=gpu:{gpuarch}:{n_gpus}"
-        if nodename:
-            sbatch_opts += f" --nodelist={nodename}"
-
-        return f"sbatch {sbatch_opts} --wrap='{cmd}'"
-
     def run(  # type:ignore
         self,
-        tag: str,
-        type: Literal["debug", "release"],
+        simcommsys_tag: str,
+        simcommsys_type: Literal["debug", "release"],
         jobs: list[SimcommsysJob],
         *,
         needs_gpu: bool,
@@ -146,16 +110,28 @@ class SlurmSimcommsysExecutor(SimcommsysExecutor):
 
         index = 0
         for job in jobs:
-            cmd = self._get_exec_cmd(
-                job.name,
-                self._get_simcommsys_cmd(tag, type, job),
-                needs_gpu=needs_gpu,
-                n_gpus=n_gpus,
-                gpuarch=gpuarch,
-                memlimit_gb=memlimit_gb,
-                timeout_mins=timeout_mins,
-                nodename=(nodelist[index] if nodelist else None),
+            # build sbatch command for this job
+            nodename = nodelist[index] if nodelist else None
+            sbatch_opts = f"--mail-user={self.email} \
+                        --job-name={job.name} \
+                        --partition={self.gpu_partition if needs_gpu else self.cpu_partition} \
+                        --ntasks=1 \
+                        --cpus-per-task=1 \
+                        --mem-per-cpu={memlimit_gb}G \
+                        --time={timeout_mins} \
+                        --output={job.name}.out \
+                        --error={job.name}.err \
+                        --account={self.account} \
+                        --mail-type=all"
+            if needs_gpu:
+                sbatch_opts += f" --gres=gpu:{gpuarch}:{n_gpus}"
+            if nodename:
+                sbatch_opts += f" --nodelist={nodename}"
+            simcommsys_cmd = self._get_simcommsys_cmd(
+                simcommsys_tag, simcommsys_type, job
             )
+            cmd = f"sbatch {sbatch_opts} --wrap='{simcommsys_cmd} -e local'"
+
             logging.debug(cmd)
             subprocess.run(cmd, shell=True)
 
@@ -164,22 +140,20 @@ class SlurmSimcommsysExecutor(SimcommsysExecutor):
 
 
 class LocalSimcommsysExecutor(SimcommsysExecutor):
-    def _get_exec_cmd(self, jobname: str, cmd: str, memlimit_gb=100) -> str:  # type: ignore
-        return cmd
-
     def run(  # type:ignore
         self,
-        tag: str,
-        type: Literal["debug", "release"],
+        simcommsys_tag: str,
+        simcommsys_type: Literal["debug", "release"],
         jobs: list[SimcommsysJob],
         *,
         memlimit_gb=100,
     ):
         for job in jobs:
-            cmd = self._get_exec_cmd(
-                job.name,
-                self._get_simcommsys_cmd(tag, type, job),
-                memlimit_gb=memlimit_gb,
+            # build command to submit to shell.
+            simcommsys_cmd = self._get_simcommsys_cmd(
+                simcommsys_tag, simcommsys_type, job
             )
+            cmd = f"ulimit -v {memlimit_gb * 1024 * 1024} && {simcommsys_cmd} -e local"
+
             logging.debug(cmd)
             subprocess.run(cmd, shell=True)
