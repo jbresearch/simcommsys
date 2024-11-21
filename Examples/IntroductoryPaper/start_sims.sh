524f14ed4b2ca1e757d81a806a3cb2b369040a4c
Mark Mizzi <mark.mizzi@um.edu.mt>
2024-11-21 11:51:19 +0100

    added master-slave configuration for IntroductoryPaper example

--- Examples/IntroductoryPaper/start_sims.sh
+++ Examples/IntroductoryPaper/start_sims.sh
@@ -3,5 +3,11 @@
 set -e
 
 cd ../../Utils
+
+# Run jobs using local simulation mode
+# poetry run simcommsys-utils run-jobs --config-file \
+#    ../Examples/IntroductoryPaper/Configurations/local-jobs.yaml
+
+# Run jobs using master-slave simulation mode with local workers
 poetry run simcommsys-utils run-jobs --config-file \
-   ../Examples/IntroductoryPaper/Configurations/local-jobs.yaml
+   ../Examples/IntroductoryPaper/Configurations/masterslave-jobs.yaml
