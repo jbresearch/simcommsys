1acae3351f450f30d46ec497cbf7a9da7da09b7e
Aaron Abela <aaron.abela@um.edu.mt>
2025-05-26 12:54:30 +0200

    Updated testgaussiancvqkd file for the Quantum Gaussian Channel

--- Test/TestGaussianCVQKDSource/testgaussiancvqkdsource.cpp
+++ Test/TestGaussianCVQKDSource/testgaussiancvqkdsource.cpp
@@ -47,89 +47,89 @@ int run_boost_tests(int argc, char* argv[]);  // Forward declaration
 
 int main(int argc, char* argv[]) {
 
-    // -------- Test 1: Generating a single coherent state --------
-    const double mean_p_mean = 0.0;
-    const double stddev_p_mean = std::sqrt(10); // 0.3162; // square root of V_A = 0.1
-    const double mean_q_mean = 0.0;
-    const double stddev_q_mean = std::sqrt(10); //0.3162; // square root of V_A = 0.1
+    // // -------- Test 1: Generating a single coherent state --------
+    // const double mean_p_mean = 0.0;
+    // const double stddev_p_mean = std::sqrt(10); // 0.3162; // square root of V_A = 0.1
+    // const double mean_q_mean = 0.0;
+    // const double stddev_q_mean = std::sqrt(10); //0.3162; // square root of V_A = 0.1
 
-    quantum_gaussian single_source(mean_q_mean, stddev_q_mean, mean_p_mean, stddev_p_mean);
-    gaussian_state state = single_source.generate_single();
+    // quantum_gaussian single_source(mean_q_mean, stddev_q_mean, mean_p_mean, stddev_p_mean);
+    // gaussian_state state = single_source.generate_single();
 
-    double q_mean = state.get_q_mean();
-    double p_mean = state.get_p_mean();
+    // double q_mean = state.get_q_mean();
+    // double p_mean = state.get_p_mean();
 
-    cout << "Test 1 - Generated a Single Gaussian state:\n";
-    std::cout << "Generated state parameters:\n";
-    std::cout << " q_mean = " << q_mean << "\n";
-    std::cout << " p_mean = " << p_mean << "\n";
+    // cout << "Test 1 - Generated a Single Gaussian state:\n";
+    // std::cout << "Generated state parameters:\n";
+    // std::cout << " q_mean = " << q_mean << "\n";
+    // std::cout << " p_mean = " << p_mean << "\n";
 
 
-    // Test 2 - Generate a sequence of coherent states with their own respective q_mean and p_mean
-    int num_states = 5;
-    libbase::size_type<libbase::vector> blocksize(num_states);
+    // // Test 2 - Generate a sequence of coherent states with their own respective q_mean and p_mean
+    // int num_states = 5;
+    // libbase::size_type<libbase::vector> blocksize(num_states);
 
-    const double mean_p_mean2 = 0.0;
-    const double stddev_p_mean2 = 10.0;
-    const double mean_q_mean2 = 0.0;
-    const double stddev_q_mean2 = 10.0;
+    // const double mean_p_mean2 = 0.0;
+    // const double stddev_p_mean2 = 10.0;
+    // const double mean_q_mean2 = 0.0;
+    // const double stddev_q_mean2 = 10.0;
 
-    auto src = std::make_unique<quantum_gaussian>(mean_q_mean2, stddev_q_mean2, mean_p_mean2, stddev_p_mean2);
-    libbase::vector<gaussian_state> gaussian_seq = src->generate_sequence(blocksize);
+    // auto src = std::make_unique<quantum_gaussian>(mean_q_mean2, stddev_q_mean2, mean_p_mean2, stddev_p_mean2);
+    // libbase::vector<gaussian_state> gaussian_seq = src->generate_sequence(blocksize);
 
-    cout << "\nTest 2 - Generate Sequence of Gaussian Coherent States\n";
-    for (int i = 0; i < gaussian_seq.size(); ++i) {
-        gaussian_state& s = gaussian_seq(i);
-        cout << "State " << i << ": q_mean = " << s.get_q_mean() << ", p_mean = " << s.get_p_mean() << "\n";
-    }
-
-    // // -------- Extra Test  --------
-    // cout << "\nTest 4 - Choosing between p and q for each generated state (50% chance)\n";
-    // libbase::vector<double> selected_quadrature(gaussian_seq.size());
-    // std::mt19937 rng(42);
-    // std::uniform_real_distribution<> dist(0.0, 1.0);
-
+    // cout << "\nTest 2 - Generate Sequence of Gaussian Coherent States\n";
     // for (int i = 0; i < gaussian_seq.size(); ++i) {
     //     gaussian_state& s = gaussian_seq(i);
-    //     selected_quadrature(i) = (dist(rng) < 0.5) ? s.get_q() : s.get_p();
+    //     cout << "State " << i << ": q_mean = " << s.get_q_mean() << ", p_mean = " << s.get_p_mean() << "\n";
     // }
 
-    // cout << "Selected values (q or p):\n";
-    // for (int i = 0; i < selected_quadrature.size(); ++i) {
-    //     cout << i << ": " << selected_quadrature(i) << "\n";
-    // }
+    // // // -------- Extra Test  --------
+    // // cout << "\nTest 4 - Choosing between p and q for each generated state (50% chance)\n";
+    // // libbase::vector<double> selected_quadrature(gaussian_seq.size());
+    // // std::mt19937 rng(42);
+    // // std::uniform_real_distribution<> dist(0.0, 1.0);
 
-    // -------- Test 4 - Transmit the GM Coherent State through a Gaussian Quantum Channel --------
-    cout << "\nTest 4 - Transmit the GM Coherent State through a Gaussian Quantum Channel without using text file/serializer \n";
-    // Step 2: Wrap them in observables
-    position_observable q_obs(q_mean);
-    momentum_observable p_obs(p_mean);
+    // // for (int i = 0; i < gaussian_seq.size(); ++i) {
+    // //     gaussian_state& s = gaussian_seq(i);
+    // //     selected_quadrature(i) = (dist(rng) < 0.5) ? s.get_q() : s.get_p();
+    // // }
 
-    std::cout << "[Before Channel] q = " << q_obs.value << ", p = " << p_obs.value << std::endl;
+    // // cout << "Selected values (q or p):\n";
+    // // for (int i = 0; i < selected_quadrature.size(); ++i) {
+    // //     cout << i << ": " << selected_quadrature(i) << "\n";
+    // // }
 
-    // Step 3: Create and configure the Gaussian quantum channel
-    gaussian_quantum_channel channel;
+    // // -------- Test 4 - Transmit the GM Coherent State through a Gaussian Quantum Channel --------
+    // cout << "\nTest 4 - Transmit the GM Coherent State through a Gaussian Quantum Channel without using text file/serializer \n";
+    // // Step 2: Wrap them in observables
+    // position_observable q_obs(q_mean);
+    // momentum_observable p_obs(p_mean);
 
-    libbase::vector<double> params;
-    params.init(6);
-    params(0) = 1.0;   // N_0
-    params(1) = 0.2;   // alpha
-    params(2) = 0.6;   // detector efficiency
-    params(3) = 70.0;  // distance in km
-    params(4) = 0.01;  // excess noise
-    params(5) = 0.1;   // electronic noise
-    channel.set_parameters(params);
+    // std::cout << "[Before Channel] q = " << q_obs.value << ", p = " << p_obs.value << std::endl;
 
-    // Step 4: Apply noise
-    channel.transmit(q_obs);
-    channel.transmit(p_obs);
-    channel.get_parameters();
+    // // Step 3: Create and configure the Gaussian quantum channel
+    // gaussian_quantum_channel channel;
 
-    channel.print_parameters();
-    // std::cout << "Transmittance: " << channel.get_parameters()(6) << "\n";
-    // std::cout << "V_N: " << channel.get_parameters()(7) << "\n";
+    // libbase::vector<double> params;
+    // params.init(6);
+    // params(0) = 1.0;   // N_0
+    // params(1) = 0.2;   // alpha
+    // params(2) = 0.6;   // detector efficiency
+    // params(3) = 70.0;  // distance in km
+    // params(4) = 0.01;  // excess noise
+    // params(5) = 0.1;   // electronic noise
+    // channel.set_parameters(params);
 
-    std::cout << "[After Channel]  q_mean = " << q_obs.value << ", p_mean = " << p_obs.value << std::endl;
+    // // Step 4: Apply noise
+    // channel.transmit(q_obs);
+    // channel.transmit(p_obs);
+    // channel.get_parameters();
+
+    // channel.print_parameters();
+    // // std::cout << "Transmittance: " << channel.get_parameters()(6) << "\n";
+    // // std::cout << "V_N: " << channel.get_parameters()(7) << "\n";
+
+    // std::cout << "[After Channel]  q_mean = " << q_obs.value << ", p_mean = " << p_obs.value << std::endl;
 
     // // -------- Test 5 - Boost Test Case --------
     cout << "\nTest 5 - Boost Test Case: \n";
@@ -204,9 +204,9 @@ BOOST_AUTO_TEST_CASE(test_gaussian_source_serialisation)
 
     //  Deserialize ss_G_QC to get the quantum_gaussian_channel
     std::unique_ptr<serializable> ptr2 = gaussian_quantum_channel::create(ss_G_QC);
-    auto* channel = dynamic_cast<gaussian_quantum_channel*>(ptr2.get());
+    auto* quantum_channel = dynamic_cast<gaussian_quantum_channel*>(ptr2.get());
 
-    channel->print_parameters();
+    quantum_channel->print_parameters();
 
     // Test case for a single state
     // std::cout << "[Before Channel] q = " << q_obs.value << ", p = " << p_obs.value << std::endl;
@@ -247,8 +247,8 @@ BOOST_AUTO_TEST_CASE(test_gaussian_source_serialisation)
         position_observable q_obs(q_in);
         momentum_observable p_obs(p_in);
 
-        channel->transmit(q_obs);
-        channel->transmit(p_obs);
+        quantum_channel->transmit(q_obs);
+        quantum_channel->transmit(p_obs);
 
         double q_out = q_obs.value;
         double p_out = p_obs.value;
