62028ea65b65b238b22fa537d10a7473f6f2b8da
Aaron Abela <aaron.abela@um.edu.mt>
2025-05-27 16:38:34 +0200

    Created separate files for the Identity Quantum channel

---
+++ Libraries/Libcomm/qkd/quantum_channel/identity_quantum_channel.cpp
@@ -0,0 +1,38 @@
+/*!
+ * \file identity_quantum_channel.cpp
+ *
+ * Copyright (c) 2025 Mark Mizzi
+ *
+ * This file is part of SimCommSys.
+ * Released under the GNU General Public License v3 or later.
+ */
+
+#include "qkd/quantum_channel/identity_quantum_channel.h"
+#include "serializer.h"
+
+using libbase::serializer;
+
+namespace libcomm {
+
+//! Return a string describing the channel
+std::string identity_quantum_channel::description() const {
+    return "Identity quantum channel";
+}
+
+//! Serialize channel to output stream
+std::ostream& identity_quantum_channel::serialize(std::ostream& sout) const {
+    return sout;
+}
+
+//! Deserialize channel from input stream
+std::istream& identity_quantum_channel::serialize(std::istream& sin) {
+    return sin;
+}
+
+//! Register with serializer system
+const serializer identity_quantum_channel::shelper(
+    "quantum_channel", "identity_quantum_channel", identity_quantum_channel::create);
+
+} // namespace libcomm
+
+
---
+++ Libraries/Libcomm/qkd/quantum_channel/identity_quantum_channel.h
@@ -0,0 +1,49 @@
+#ifndef __identity_quantum_channel_h
+#define __identity_quantum_channel_h
+
+#include "qkd/quantum_channel.h"
+
+namespace libcomm {
+
+/*!
+ * \brief   Identity Quantum channel.
+ * \author  Mark Mizzi
+ *
+ * Supports every observable.
+ * Does not affect any observable, but leaves them as they are.
+ */
+class identity_quantum_channel : public quantum_channel
+{
+public:
+    //! \name Visitor interface methods for observables
+    virtual void transmit(position_observable&) const {}
+    virtual void transmit(momentum_observable&) const {}
+    virtual void transmit(spin_computational&) const {}
+    virtual void transmit(spin_hadamard&) const {}
+    //! @}
+
+    void seedfrom(libbase::random& r) override {}
+
+    /*! \name Parameter handling */
+    //! Set the characteristic parameters
+    void set_parameters(const libbase::vector<double>& x) override {}
+    //! Get the characteristic parameters
+    libbase::vector<double> get_parameters() const override
+    {
+        libbase::vector<double> params;
+        params.init(0);
+        return params;
+    }
+    int get_num_params() const override { return 0; }
+    // @}
+
+    // Description
+    std::string description() const override;
+
+    // Serialization Support
+    DECLARE_SERIALIZER(identity_quantum_channel)
+};
+
+}
+
+#endif // __identity_quantum_channel_h
