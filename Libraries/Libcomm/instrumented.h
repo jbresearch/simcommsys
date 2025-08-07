df3c14869a3aa1c4de7cda3f5f25e2676074e582
Johann A. Briffa <johann.briffa@um.edu.mt>
2025-08-07 09:25:03 +0200

    Interface fix - remove unused parameter

--- Libraries/Libcomm/instrumented.h
+++ Libraries/Libcomm/instrumented.h
@@ -123,7 +123,7 @@ public:
     //! Add a single, new timing, or if a timing with same name already exists,
     //! accumulate
     void
-    add_or_accumulate_timer(double time, const std::string& name, int count = 1)
+    add_or_accumulate_timer(double time, const std::string& name)
     {
         auto m_names_it = m_names.begin();
         auto m_counts_it = m_counts.begin();
