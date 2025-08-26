5f310da518ad4f2c7bda57bd5c0df098728aef19
Johann A. Briffa <johann.briffa@um.edu.mt>
2025-08-26 16:07:22 +0200

    Simplify output and show summary for multiple authors

--- Scripts/Development/getloc.sh
+++ Scripts/Development/getloc.sh
@@ -2,7 +2,9 @@
 
 # main process
 echo "Analysis for all C/C++ code:"
-find . -type f \( -name '*.cpp' -or -name '*.c' -or -name '*.cu' -or -name '*.h' \) -print0 |xargs -0 c_count
+find . -type f \( -name '*.cpp' -or -name '*.c' -or -name '*.cu' -or -name '*.h' \) -print0 |xargs -0 c_count |tail -1
 
-echo "Analysis for code written by S. Wesemeyer:"
-find . -type f \( -name '*.cpp' -or -name '*.c' -or -name '*.cu' -or -name '*.h' \) -print0 |xargs -0 grep -i wesemeyer -l |xargs c_count
+for developer in briffa wesemeyer mizzi abela; do
+    echo "Analysis for code written by ${developer}:"
+    find . -type f \( -name '*.cpp' -or -name '*.c' -or -name '*.cu' -or -name '*.h' \) -print0 |xargs -0 grep -i ${developer} -l |xargs c_count |tail -1
+done
