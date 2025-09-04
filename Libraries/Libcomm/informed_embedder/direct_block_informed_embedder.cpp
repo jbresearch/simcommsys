f8427ccb6d51e58c99d8b66575de9515e69e1fac
Johann A. Briffa <johann.briffa@um.edu.mt>
2025-09-04 12:58:00 +0200

    First draft of position-independent block informed embedder

---
+++ Libraries/Libcomm/informed_embedder/direct_block_informed_embedder.cpp
@@ -0,0 +1,263 @@
+/*!
+ * \file
+ *
+ * Copyright (c) 2025 Johann A. Briffa
+ *
+ * This file is part of SimCommSys.
+ *
+ * SimCommSys is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * SimCommSys is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with SimCommSys.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "direct_block_informed_embedder.h"
+#include <cstdlib>
+#include <sstream>
+#include <vectorutils.h>
+
+namespace libcomm
+{
+
+using libbase::matrix;
+using libbase::vector;
+
+// *** Vector position-independent block_informed_embedder ***
+
+// Block modem operations
+
+template <class S, class dbl>
+void
+direct_block_informed_embedder<S, vector, dbl>::doembed(const int N,
+                                                        const vector<int>& data,
+                                                        const vector<S>& host,
+                                                        vector<S>& tx)
+{
+    // Check validity
+    assertalways(data.size() == this->input_block_size());
+    assertalways(host.size() == this->input_block_size());
+    assertalways(N == this->num_symbols());
+    // Inherit sizes
+    const int tau = this->input_block_size();
+    // Initialize results vector
+    tx.init(tau);
+    // Modulate encoded stream
+    for (int i = 0; i < tau; i++) {
+        tx(i) = implementation->embed(data(i), host(i));
+    }
+}
+
+template <class S, class dbl>
+void
+direct_block_informed_embedder<S, vector, dbl>::doextract(
+    const channel<S, vector>& chan,
+    const vector<S>& rx,
+    const vector<S>& reference,
+    vector<array1d_t>& ptable)
+{
+    // Check validity
+    assertalways(rx.size() == this->input_block_size());
+    // Inherit sizes
+    const int tau = this->input_block_size();
+    const int M = this->num_symbols();
+    // Allocate space for temporary results
+    vector<vector<double>> ptable_double;
+    {
+        // Create a set of all possible transmitted symbols, at each timestep
+        vector<vector<S>> tx;
+        libbase::allocate(tx, tau, M);
+        for (int t = 0; t < tau; t++) {
+            for (int x = 0; x < M; x++) {
+                tx(t)(x) = implementation->embed(x, reference(t));
+            }
+        }
+
+        // Work out the probabilities of each possible signal
+        chan.receive(tx, rx, ptable_double);
+    }
+    // Convert result
+    ptable = ptable_double;
+}
+
+// Description
+
+template <class S, class dbl>
+std::string
+direct_block_informed_embedder<S, vector, dbl>::description() const
+{
+    std::ostringstream sout;
+    sout << "Vector " << implementation->description();
+    return sout.str();
+}
+
+// Serialization Support
+
+template <class S, class dbl>
+std::ostream&
+direct_block_informed_embedder<S, vector, dbl>::serialize(
+    std::ostream& sout) const
+{
+    sout << implementation;
+    return sout;
+}
+
+template <class S, class dbl>
+std::istream&
+direct_block_informed_embedder<S, vector, dbl>::serialize(std::istream& sin)
+{
+    sin >> libbase::eatcomments >> implementation >> libbase::verify;
+    return sin;
+}
+
+// *** Matrix position-independent block_informed_embedder ***
+
+// Block modem operations
+
+template <class S, class dbl>
+void
+direct_block_informed_embedder<S, matrix, dbl>::doembed(const int N,
+                                                        const matrix<int>& data,
+                                                        const matrix<S>& host,
+                                                        matrix<S>& tx)
+{
+    // Check validity
+    assertalways(data.size() == this->input_block_size());
+    assertalways(host.size() == this->input_block_size());
+    assertalways(N == this->num_symbols());
+
+    // Inherit sizes
+    const int rows = this->input_block_size().rows();
+    const int cols = this->input_block_size().cols();
+
+    // Initialize results matrix
+    tx.init(this->input_block_size());
+
+    // Modulate encoded stream
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            tx(i, j) = implementation->embed(data(i, j), host(i, j));
+        }
+    }
+}
+
+template <class S, class dbl>
+void
+direct_block_informed_embedder<S, matrix, dbl>::doextract(
+    const channel<S, matrix>& chan,
+    const matrix<S>& rx,
+    const matrix<S>& reference,
+    matrix<array1d_t>& ptable)
+{
+    // Check validity
+    assertalways(rx.size() == this->input_block_size());
+
+    // Inherit sizes
+    const int rows = this->input_block_size().rows();
+    const int cols = this->input_block_size().cols();
+    const int M = this->num_symbols();
+
+    // Allocate space for temporary results
+    matrix<vector<double>> ptable_double;
+    {
+        // Create a set of all possible transmitted symbols, at each timestep
+        matrix<vector<S>> tx;
+        libbase::allocate(tx, rows, cols, M);
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                for (int x = 0; x < M; x++) {
+                    tx(i, j)(x) =
+                        implementation->embed(x, reference(i, j));
+                }
+            }
+        }
+
+        // Work out the probabilities of each possible signal
+        chan.receive(tx, rx, ptable_double);
+    }
+
+    // Convert result
+    ptable = ptable_double;
+}
+
+// Description
+
+template <class S, class dbl>
+std::string
+direct_block_informed_embedder<S, matrix, dbl>::description() const
+{
+    std::ostringstream sout;
+    sout << "Matrix " << implementation->description();
+    return sout.str();
+}
+
+// Serialization Support
+
+template <class S, class dbl>
+std::ostream&
+direct_block_informed_embedder<S, matrix, dbl>::serialize(
+    std::ostream& sout) const
+{
+    sout << implementation;
+    return sout;
+}
+
+template <class S, class dbl>
+std::istream&
+direct_block_informed_embedder<S, matrix, dbl>::serialize(std::istream& sin)
+{
+    sin >> libbase::eatcomments >> implementation >> libbase::verify;
+    return sin;
+}
+
+} // namespace libcomm
+
+namespace libcomm
+{
+
+// Explicit Realizations
+#include <boost/preprocessor/seq/enum.hpp>
+#include <boost/preprocessor/seq/for_each_product.hpp>
+#include <boost/preprocessor/stringize.hpp>
+
+using libbase::matrix;
+using libbase::serializer;
+using libbase::vector;
+
+// clang-format off
+#define SYMBOL_TYPE_SEQ \
+   (int)(float)(double)
+#define CONTAINER_TYPE_SEQ \
+   (vector)(matrix)
+#define REAL_TYPE_SEQ \
+   (double)
+
+/* Serialization string: direct_block_informed_embedder<type,container,real>
+ * where:
+ *      type = int | float | double
+ *      container = vector | matrix
+ *      real = double
+ *              [real is the interface arithmetic type]
+ */
+#define INSTANTIATE(r, args) \
+      template class direct_block_informed_embedder<BOOST_PP_SEQ_ENUM(args)>; \
+      template <> \
+      const serializer direct_block_informed_embedder<BOOST_PP_SEQ_ENUM(args)>::shelper( \
+            "block_informed_embedder", \
+            "direct_block_informed_embedder<" BOOST_PP_STRINGIZE(BOOST_PP_SEQ_ELEM(0,args)) "," \
+            BOOST_PP_STRINGIZE(BOOST_PP_SEQ_ELEM(1,args)) "," \
+            BOOST_PP_STRINGIZE(BOOST_PP_SEQ_ELEM(2,args)) ">", \
+            direct_block_informed_embedder<BOOST_PP_SEQ_ENUM(args)>::create);
+// clang-format on
+
+BOOST_PP_SEQ_FOR_EACH_PRODUCT(
+    INSTANTIATE, (SYMBOL_TYPE_SEQ)(CONTAINER_TYPE_SEQ)(REAL_TYPE_SEQ))
+
+} // namespace libcomm
---
+++ Libraries/Libcomm/informed_embedder/direct_block_informed_embedder.h
@@ -0,0 +1,133 @@
+/*!
+ * \file
+ *
+ * Copyright (c) 2025 Johann A. Briffa
+ *
+ * This file is part of SimCommSys.
+ *
+ * SimCommSys is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * SimCommSys is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with SimCommSys.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __direct_block_informed_embedder_h
+#define __direct_block_informed_embedder_h
+
+#include "block_informed_embedder.h"
+#include "informed_embedder.h"
+
+namespace libcomm
+{
+
+/*!
+ * \brief   Position-Independent Blockwise Informed Data Embedder/Extractor.
+ * \author  Johann Briffa
+ *
+ * This class is a template definition for position-independent block informed
+ * embedders; this needs to be specialized for actual use. Template parameter
+ * defaults are provided here.
+ */
+
+template <class S,
+          template <class> class C = libbase::vector,
+          class dbl = double>
+class direct_block_informed_embedder : public block_informed_embedder<S, C, dbl>
+{
+};
+
+/*!
+ * \brief   Position-Independent Vector Informed Data Embedder/Extractor
+ * \author  Johann Briffa
+ *
+ * Vector implementation of a position-independent block informed embedder.
+ */
+
+template <class S, class dbl>
+class direct_block_informed_embedder<S, libbase::vector, dbl>
+    : public block_informed_embedder<S, libbase::vector, dbl>
+{
+public:
+    /*! \name Type definitions */
+    typedef libbase::vector<dbl> array1d_t;
+    // @}
+private:
+    /*! \name User-defined parameters */
+    std::shared_ptr<informed_embedder<S>>
+        implementation; //! Implementation of embedding/extraction functions
+                        // @}
+protected:
+    // Interface with derived classes
+    void doembed(const int N,
+                 const libbase::vector<int>& data,
+                 const libbase::vector<S>& host,
+                 libbase::vector<S>& tx);
+    void doextract(const channel<S, libbase::vector>& chan,
+                   const libbase::vector<S>& rx,
+                   const libbase::vector<S>& reference,
+                   libbase::vector<array1d_t>& ptable);
+
+public:
+    // Informative functions
+    int num_symbols() const { return implementation->num_symbols(); }
+
+    // Description
+    std::string description() const;
+
+    // Serialization Support
+    DECLARE_SERIALIZER(direct_block_informed_embedder)
+};
+
+/*!
+ * \brief   Position-Independent Matrix Informed Data Embedder/Extractor
+ * \author  Johann Briffa
+ *
+ * Matrix implementation of a position-independent block informed embedder.
+ */
+
+template <class S, class dbl>
+class direct_block_informed_embedder<S, libbase::matrix, dbl>
+    : public block_informed_embedder<S, libbase::matrix, dbl>
+{
+public:
+    /*! \name Type definitions */
+    typedef libbase::vector<dbl> array1d_t;
+    // @}
+private:
+    /*! \name User-defined parameters */
+    std::shared_ptr<informed_embedder<S>>
+        implementation; //! Implementation of embedding/extraction functions
+                        // @}
+protected:
+    // Interface with derived classes
+    void doembed(const int N,
+                 const libbase::matrix<int>& data,
+                 const libbase::matrix<S>& host,
+                 libbase::matrix<S>& tx);
+    void doextract(const channel<S, libbase::matrix>& chan,
+                   const libbase::matrix<S>& rx,
+                   const libbase::matrix<S>& reference,
+                   libbase::matrix<array1d_t>& ptable);
+
+public:
+    // Informative functions
+    int num_symbols() const { return implementation->num_symbols(); }
+
+    // Description
+    std::string description() const;
+
+    // Serialization Support
+    DECLARE_SERIALIZER(direct_block_informed_embedder)
+};
+
+} // namespace libcomm
+
+#endif
