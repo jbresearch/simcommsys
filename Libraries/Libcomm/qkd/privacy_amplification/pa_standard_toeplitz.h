015f6772f4bbb2f27bd0a928ef6b7116c94e3d02
Aaron Abela <aaron.abela@um.edu.mt>
2025-09-11 16:15:51 +0200

    Added setters to set L, N and alphabet size for the standard Toeplitz PA method.

--- Libraries/Libcomm/qkd/privacy_amplification/pa_standard_toeplitz.h
+++ Libraries/Libcomm/qkd/privacy_amplification/pa_standard_toeplitz.h
@@ -53,6 +53,12 @@ class pa_standard_toeplitz: public privacy_amplification_base<T>
         int get_N() {return N;}
         int get_alphabet_size() {return alphabet_size;}
 
+        // setters to set the sizes of L, N and alphabet_size
+        void set_L(int l) {this->L = l;}
+        void set_N(int n) {this->N = n;}
+        void set_alphabet_size(int q) {this->alphabet_size = q;}
+
+
 
         DECLARE_SERIALIZER(pa_standard_toeplitz<T>)
 };
