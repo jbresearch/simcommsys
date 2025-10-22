70a199d496aba612ad70daeac60ebf25f02b1d9d
Aaron Abela <aaron.abela@um.edu.mt>
2025-10-22 17:05:14 +0200

    Created the observable for the hadamard basis for Bob.

---
+++ Libraries/Libcomm/qkd/observable/hadamard_observable.h
@@ -0,0 +1,146 @@
+/*!
+ * \file
+ *
+ * Copyright (c) 2025 Aaron Abela
+ *
+ * This file is part of SimCommSys.
+ *
+ * SimCommSys is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * SimCommSys is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with SimCommSys.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*!
+ * \brief Real Hadamard (X-Basis) Observable
+ * \author Aaron Abela
+ *
+ * An observable that performs a probabilistic quantum measurement
+ * on a qubit in the X-Basis (Hadamard).
+ *
+ * Reference: https://quantum-education-modules.readthedocs.io/en/latest/introductory/qubits/measurement.html
+ */
+
+#ifndef __hadamard_observable_h
+#define __hadamard_observable_h
+
+#include "qkd/observable.h"
+#include "qkd/quantum_channel.h"
+#include "qkd/quantum_state.h"
+#include "random.h"
+#include <cmath>
+#include <complex>
+#include <stdexcept>
+
+namespace libcomm
+{
+class qubit;
+class entangled_qubit_pair;
+class epr_beam;
+
+class hadamard_observable : public observable<bool>
+{
+private:
+    // 'mutable' is correct! It allows 'measure' to be const.
+    mutable libbase::randgen rng;
+
+    // This member will store the noise parameter from the depolarizing quantum channel.
+    double qber;
+
+public:
+    // Constructor
+    hadamard_observable() : rng(), qber(0.0) {}
+
+    void seedfrom(libbase::random& r)
+    {
+        this->rng.seed(r.ival());
+    }
+
+    // Noise is added from the depolarising quantum channel from the transmit method.
+    void set_qber(double qber_val) { qber = qber_val; }
+
+    bool measure(qubit& state) const override
+    {
+
+        /*
+        To measure in the Hadamard basis (|+⟩, |−⟩), we first express the state in that basis:
+
+        |ψ⟩ = α|0⟩ + β|1⟩ = c₊|+⟩ + c₋|−⟩
+
+        Where:
+        c₊ = ⟨+|ψ⟩ = (α + β) / √2
+        c₋ = ⟨−|ψ⟩ = (α - β) / √2
+
+        The probability of measuring |+⟩ is |c₊|²
+        */
+
+        // Perform Ideal, Noiseless measurement
+        const double inv_sqrt2 = 1.0 / std::sqrt(2.0);
+
+        // Get Z-Basis amplitudes
+        std::complex<double> alpha = state.get_comp_basis_0();
+        std::complex<double> beta = state.get_comp_basis_1();
+
+        // Convert to X-Basis amplitudes.
+        // Only the amplitude for the |+> state (bit 0) is required.
+        // alpha_X = (alpha + beta) / sqrt(2)
+        std::complex<double> alpha_X = (alpha + beta) * inv_sqrt2;
+
+        // Calculate probability of measuring |+> (bit 0)
+        double prob_plus = std::norm(alpha_X);
+
+        /* To simulate a measurement, generate a uniform random number between 0 and 1.
+        If the number is less than prob_plus, return 0 (i.e., collapse to |+⟩); else, return 1 (collapse to |−⟩).
+        This follows the Born rule in simulation form.
+        */
+
+        bool ideal_result;
+        if (rng.fval_halfopen() < prob_plus) {
+            ideal_result = false; // Ideal result is 0 (state |+>)
+        } else {
+            ideal_result = true;  // Ideal result is 1 (state |->)
+        }
+
+        // --- Apply Channel Noise (QBER) ---
+        // Get a new random number to check for a bit-flip error.
+        if (rng.fval_halfopen() < qber) {
+            return !ideal_result; // A bit-flip error occurs
+        } else {
+            return ideal_result;  // No error
+        }
+    }
+
+    void transmit(quantum_channel& c) override { c.transmit(*this); }
+
+    bool measure(gaussian_state&) const override
+    {
+        failwith(
+            "hadamard_observable does not support gaussian_state measurement.");
+        return 0;
+    }
+
+    bool measure(entangled_qubit_pair&, int) const override
+    {
+        failwith("hadamard_observable does not support entangled qubit "
+                 "pair measurement.");
+        return 0;
+    }
+
+    bool measure(epr_beam&, int) const override
+    {
+        failwith(
+            "hadamard_observable does not support EPR beam measurement.");
+        return 0;
+    }
+};
+} // namespace libcomm
+
+#endif // __hadamard_observable_h
